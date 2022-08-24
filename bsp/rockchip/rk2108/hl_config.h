772c92f80e3011d996a7c7cef274f49b9b4aae82
yangxianyun <rd52@hollyland-tech.com>
2022-08-24 15:06:15 +0800

    💄 样式规范（clang&git）：添加规范相关文件  1. 添加了clang-format文件用来进行代码格式化  2. 添加了新的gitignore文件，详见具体文件 结束 🔧 系统调整（编译相关）：调整增加了编译相关的一些内容  1. 增加了A6902的板子配置，里面存放一些后续不频繁的更改  2. 增加了hl_config.h文件，用来存放经常更改的内容如内部版本号等  3. 增加了编译分发相关的脚本build_distribution.sh，用来配合jinkins进行自动化编译分发版本 结束

---
+++ bsp/rockchip/rk2108/hl_config.h
@@ -0,0 +1,46 @@
+#define A6902_TX_HL 0
+#define A6902_RX_HL_CH 1
+#define A6902_RX_HL_EN 1
+#define A6902_TX_JF 0
+#define A6902_RX_JF_CH 0
+#define A6902_RX_JF_EN 0
+// 用于指示型号配置结束，注意该文件不可以加入文件头部注释，除了管理员外不可以在该行自行添加任何注释，否则编译会出问题
+// indicate end of config, do not add any comment in this line except the maintainer
+
+#if A6902_TX_HL
+
+#define A6902_VERSION "V1.0.0.0"
+#define A6902_LANGUAGE "CN"
+
+#elif A6902_TX_JF
+
+#define A6902_VERSION "V1.2.3.4"
+#define A6902_LANGUAGE "CN"
+
+#elif A6902_RX_HL_CH
+
+#define A6902_VERSION "V1.0.0.0"
+#define A6902_LANGUAGE "CN"
+
+#elif A6902_RX_JF_CH
+
+#define A6902_VERSION "V1.0.0.0"
+#define A6902_LANGUAGE "CN"
+
+#elif A6902_RX_HL_EN
+
+#define A6902_VERSION "V1.0.0.0"
+#define A6902_LANGUAGE "EN"
+
+#elif A6902_RX_JF_EN
+
+#define A6902_VERSION "V1.0.0.0"
+#define A6902_LANGUAGE "EN"
+
+#endif
+
+// TX返回0 RX返回1
+#define HL_GET_DEVICE_TYPE() (A6902_TX_HL | A6902_TX_JF)
+
+// HL返回0 飓风返回1
+#define HL_GET_LOGO_TYPE() (A6902_TX_JF | A6902_RX_JF_CH | A6902_RX_JF_EN)
