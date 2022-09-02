daf1eae80fbc26f2d5214d2c1c809299c72d5ed4
lisonglin <songlin.li@hollyland-tech.com>
2022-09-02 16:55:33 +0800

    📝 添加（苹果认证模块）  1、添加目录：HL_A6902/module/inc和HL_A6902/module/src  2、添加文件：hl_mod_apple_auth.c和hl_mod_apple_auth.h  3、创建两个线程，分别执行苹果认证IAP2协议流程和外部接口通信EAP协议处理。 结束

---
+++ applications/HL_A6902/module/inc/hl_mod_apple_auth.h
@@ -0,0 +1,108 @@
+/**
+ * @file hl_mod_apple_auth.h
+ * @author lisonglin (songlin.li@hollyland-tech.com)
+ * @brief 
+ * @version 1.0
+ * @date 2022-09-02
+ * 
+ * @copyright Copyright (c) 2022 hollyland
+ * 
+ * @par 修改日志:
+ * <table>
+ * <tr><th>Date           <th>Version  <th>Author         <th>Description
+ * <tr><td>2022-09-02     <td>v1.0     <td>lisonglin     <td>内容
+ * </table>
+ * 
+ */
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __HL_MOD_IAP2_H__
+#define __HL_MOD_IAP2_H__
+
+/* Includes ------------------------------------------------------------------*/
+#include <rtthread.h>
+
+/* typedef -------------------------------------------------------------------*/
+/* define --------------------------------------------------------------------*/
+#define IAP2_THREAD_STACK_SIZE 512
+#define IAP2_THREAD_PRIORITY 6
+#define IAP2_THREAD_TIMESLICE 10
+#define EAP_THREAD_STACK_SIZE 512
+#define EAP_THREAD_PRIORITY 7
+#define EAP_THREAD_TIMESLICE 10
+
+/* variables -----------------------------------------------------------------*/
+/* Private function(only *.c)  -----------------------------------------------*/
+/* Exported functions --------------------------------------------------------*/
+/**
+ * hl_mod_apple_auth_init
+ * @brief 苹果认证初始化两个线程，申请相应资源
+ * @return int 成功 0 | 失败 非0
+ * @date 2022-09-02
+ * @author lisonglin (songlin.li@hollyland-tech.com)
+ * 
+ * @details 
+ * @note 
+ * @par 修改日志:
+ * <table>
+ * <tr><th>Date             <th>Author         <th>Description
+ * <tr><td>2022-09-02      <td>lisonglin     <td>新建
+ * </table>
+ */
+int hl_mod_apple_auth_init();
+
+/**
+ * hl_mod_apple_auth_deinit
+ * @brief 苹果认证去初始化，释放资源
+ * @return int 成功 0 | 失败 非0
+ * @date 2022-09-02
+ * @author lisonglin (songlin.li@hollyland-tech.com)
+ * 
+ * @details 
+ * @note 
+ * @par 修改日志:
+ * <table>
+ * <tr><th>Date             <th>Author         <th>Description
+ * <tr><td>2022-09-02      <td>lisonglin     <td>新建
+ * </table>
+ */
+int hl_mod_apple_auth_deinit();
+
+/**
+ * hl_mod_apple_auth_start
+ * @brief 启动已初始化的线程，循环执行MFI认证、EAP通信、电源管理
+ * @return int 成功 0 | 失败 非0
+ * @date 2022-09-02
+ * @author lisonglin (songlin.li@hollyland-tech.com)
+ * 
+ * @details 
+ * @note 
+ * @par 修改日志:
+ * <table>
+ * <tr><th>Date             <th>Author         <th>Description
+ * <tr><td>2022-09-02      <td>lisonglin     <td>新建
+ * </table>
+ */
+int hl_mod_apple_auth_start();
+
+/**
+ * hl_mod_apple_auth_stop
+ * @brief 停止线程
+ * @return int 成功 0 | 失败 非0
+ * @date 2022-09-02
+ * @author lisonglin (songlin.li@hollyland-tech.com)
+ * 
+ * @details 
+ * @note 
+ * @par 修改日志:
+ * <table>
+ * <tr><th>Date             <th>Author         <th>Description
+ * <tr><td>2022-09-02      <td>lisonglin     <td>新建
+ * </table>
+ */
+int hl_mod_apple_auth_stop();
+
+#endif
+
+/*
+ * EOF
+ */
