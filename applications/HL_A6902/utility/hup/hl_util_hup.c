e9d6e548195b6bc25818188d78be671d7ca710ed
lisonglin <songlin.li@hollyland-tech.com>
2022-09-13 11:57:25 +0000

    ✏️ 修改（utility）：修改HUP协议工具   1、删除hl_util_hup_p结构体指针   2、新建hup_handle结构体   3、更改init函数传入参数，取消malloc申请内存   4、修改hup协议帧结构体格式长度为data_len_h和data_len_l两个字节表示 结束

--- applications/HL_A6902/utility/hup/hl_util_hup.c
+++ applications/HL_A6902/utility/hup/hl_util_hup.c
@@ -1,45 +1,54 @@
 #include "hl_util_hup.h"
 #include "hl_util_xor_calc.h"
+#include <stdlib.h>
+#include <string.h>
 
-hup_protocol_handle_p hl_util_hup_protocol_init(hup_role_em role, hup_timer_state_em timer_state, uint8_t* buf_addr,
-                                                uint16_t buf_size, uint32_t (*get_time_stamp)(void),
-                                                void (*hup_success_handle_func)(hup_protocol_type_t))
+int hl_util_hup_init(hl_util_hup_t* hup_ptr, uint8_t* frame_data_addr, uint32_t (*get_time_stamp)(void),
+                     void (*hup_success_handle_cb)(hup_protocol_type_t))
 {
-    if ((NULL == hup_success_handle_func) || (NULL == buf_addr)) {
-        HL_UTIL_HUP_DBG("[ERROR] hup protocol init arg\n");
-        return NULL;
-    }
-
-    hup_protocol_handle_p hup = (hup_protocol_handle_p)malloc(sizeof(hup_protocol_handle_t));
-    memset(hup, 0, sizeof(hup_protocol_handle_t));
-
-    hup->role                    = role;
-    hup->state                   = EM_HUP_STATE_HDR_H;
-    hup->timer_state             = timer_state;
-    hup->max_timeout             = HUP_MAX_TIMEOUT;
-    hup->get_time_stamp          = get_time_stamp;
-    hup->hup_success_handle_func = hup_success_handle_func;
-    hup->buf_len                 = buf_size;
-    hup->hup_frame.data_addr     = buf_addr;
-
-    return hup;
-}
-
-int hl_util_hup_protocol_deinit(hup_protocol_handle_p hup)
-{
-    if (NULL == hup) {
+    if (NULL == hup_success_handle_cb) {
+        HL_UTIL_HUP_DBG("[ERROR] hup init hup_success_handle_cb arg is NULL\n");
         return -1;
     }
 
-    hup->get_time_stamp          = NULL;
-    hup->hup_success_handle_func = NULL;
-    free(hup);
+    // 检查hup_handle结构体是否赋予初始值
+    if ((hup_ptr->hup_handle.role != EM_HUP_ROLE_MASTER) && (hup_ptr->hup_handle.role != EM_HUP_ROLE_SLAVE)) {
+        HL_UTIL_HUP_DBG("[ERROR] hup init handle role arg no init value\n");
+        return -1;
+    }
+    if ((hup_ptr->hup_handle.timer_state != EM_HUP_TIMER_ENABLE)
+        && (hup_ptr->hup_handle.timer_state != EM_HUP_TIMER_DISABLE)) {
+        HL_UTIL_HUP_DBG("[ERROR] hup init handle timer_state arg no init value\n");
+        return -1;
+    }
+    if (hup_ptr->hup_handle.frame_data_len == 0) {
+        HL_UTIL_HUP_DBG("[ERROR] hup init handle frame_data_len arg no init value\n");
+        return -1;
+    }
+
+    // 初始化hup结构体变量值
+    hup_ptr->state                 = EM_HUP_STATE_HDR_H;
+    hup_ptr->max_timeout           = HUP_MAX_TIMEOUT;
+    hup_ptr->get_time_stamp        = get_time_stamp;
+    hup_ptr->hup_success_handle_cb = hup_success_handle_cb;
 
     return 0;
 }
 
-int hl_util_hup_protocol_encode(uint8_t role, uint8_t cmd, uint8_t* frame_buf, uint16_t buf_len, uint8_t* data_addr,
-                                uint16_t data_len)
+int hl_util_hup_deinit(hl_util_hup_t* hup_ptr)
+{
+    if (NULL == hup_ptr) {
+        return -1;
+    }
+
+    hup_ptr->get_time_stamp        = NULL;
+    hup_ptr->hup_success_handle_cb = NULL;
+
+    return 0;
+}
+
+int hl_util_hup_encode(hup_role_em role, uint8_t cmd, uint8_t* frame_buf, uint16_t buf_len, uint8_t* data_addr,
+                       uint16_t data_len)
 {
     if ((NULL == frame_buf) | (NULL == data_addr)) {
         HL_UTIL_HUP_DBG("[ERROR] hup protocol encode arg\n");
@@ -66,84 +75,86 @@ int hl_util_hup_protocol_encode(uint8_t role, uint8_t cmd, uint8_t* frame_buf, u
     return data_len + 6;
 }
 
-int hl_util_hup_protocol_decode(hup_protocol_handle_p hup, uint8_t data_byte)
+int hl_util_hup_decode(hl_util_hup_t* hup_ptr, uint8_t data_byte)
 {
-    if (NULL == hup) {
+    if (NULL == hup_ptr) {
         HL_UTIL_HUP_DBG("[ERROR] hup protocol decode arg\n");
         return -1;
     }
 
-    if (hup->timer_state == EM_HUP_ENABLE) {
-        if (hup->state == EM_HUP_STATE_HDR_H) {
-            hup->cur_timeout_val  = 0;
-            hup->last_timeout_val = 0;
+    if (hup_ptr->hup_handle.timer_state == EM_HUP_TIMER_ENABLE) {
+        if (hup_ptr->state == EM_HUP_STATE_HDR_H) {
+            hup_ptr->cur_timeout_val  = 0;
+            hup_ptr->last_timeout_val = 0;
         } else {
-            hup->cur_timeout_val = hup->get_time_stamp();
+            hup_ptr->cur_timeout_val = hup_ptr->get_time_stamp();
         }
 
-        if ((hup->cur_timeout_val - hup->last_timeout_val) > hup->max_timeout) {
-            hup->state = EM_HUP_STATE_HDR_H;
+        if ((hup_ptr->cur_timeout_val - hup_ptr->last_timeout_val) > hup_ptr->max_timeout) {
+            hup_ptr->state = EM_HUP_STATE_HDR_H;
         } else {
-            hup->last_timeout_val = hup->cur_timeout_val;
+            hup_ptr->last_timeout_val = hup_ptr->cur_timeout_val;
         }
     }
 
-    switch (hup->state) {
+    switch (hup_ptr->state) {
         case EM_HUP_STATE_HDR_H:
-            if (((hup->role == EM_HUP_ROLE_MASTER) && (data_byte == HUP_FRAME_CLIENT_HDR_H))
-                || ((hup->role == EM_HUP_ROLE_SLAVE) && (data_byte == HUP_FRAME_HOST_HDR_H))) {
-                hup->hup_frame.hdr_h = data_byte;
-                hup->state           = EM_HUP_STATE_HDR_L;
-                hup->xor8            = data_byte;
-                hup->read_data_len   = 0;
-                if (hup->timer_state == EM_HUP_ENABLE) {
-                    hup->last_timeout_val = hup->get_time_stamp();
+            if (((hup_ptr->hup_handle.role == EM_HUP_ROLE_MASTER) && (data_byte == HUP_FRAME_CLIENT_HDR_H))
+                || ((hup_ptr->hup_handle.role == EM_HUP_ROLE_SLAVE) && (data_byte == HUP_FRAME_HOST_HDR_H))) {
+                hup_ptr->hup_frame.hdr_h = data_byte;
+                hup_ptr->state           = EM_HUP_STATE_HDR_L;
+                hup_ptr->xor8            = data_byte;
+                hup_ptr->read_data_len   = 0;
+                if (hup_ptr->hup_handle.timer_state == EM_HUP_TIMER_ENABLE) {
+                    hup_ptr->last_timeout_val = hup_ptr->get_time_stamp();
                 }
             }
             break;
 
         case EM_HUP_STATE_HDR_L:
-            if (((hup->role == EM_HUP_ROLE_MASTER) && (data_byte == HUP_FRAME_CLIENT_HDR_L))
-                || ((hup->role == EM_HUP_ROLE_SLAVE) && (data_byte == HUP_FRAME_HOST_HDR_L))) {
-                hup->hup_frame.hdr_l = data_byte;
-                hup->state           = EM_HUP_STATE_CMD;
-                hup->xor8            = hup->xor8 ^ data_byte;
+            if (((hup_ptr->hup_handle.role == EM_HUP_ROLE_MASTER) && (data_byte == HUP_FRAME_CLIENT_HDR_L))
+                || ((hup_ptr->hup_handle.role == EM_HUP_ROLE_SLAVE) && (data_byte == HUP_FRAME_HOST_HDR_L))) {
+                hup_ptr->hup_frame.hdr_l = data_byte;
+                hup_ptr->state           = EM_HUP_STATE_CMD;
+                hup_ptr->xor8            = hup_ptr->xor8 ^ data_byte;
             } else {
-                hup->state = EM_HUP_STATE_HDR_H;
+                hup_ptr->state = EM_HUP_STATE_HDR_H;
             }
             break;
 
         case EM_HUP_STATE_CMD:
-            hup->hup_frame.cmd = data_byte;
-            hup->state         = EM_HUP_STATE_DATA_LEN_H;
-            hup->xor8          = hup->xor8 ^ data_byte;
+            hup_ptr->hup_frame.cmd = data_byte;
+            hup_ptr->state         = EM_HUP_STATE_DATA_LEN_H;
+            hup_ptr->xor8          = hup_ptr->xor8 ^ data_byte;
             break;
 
         case EM_HUP_STATE_DATA_LEN_H:
-            hup->hup_frame.data_len = (data_byte << 8);
-            hup->state              = EM_HUP_STATE_DATA_LEN_L;
-            hup->xor8               = hup->xor8 ^ data_byte;
+            hup_ptr->hup_frame.data_len_h = data_byte;
+            hup_ptr->frame_data_len       = (data_byte << 8);
+            hup_ptr->state                = EM_HUP_STATE_DATA_LEN_L;
+            hup_ptr->xor8                 = hup_ptr->xor8 ^ data_byte;
             break;
 
         case EM_HUP_STATE_DATA_LEN_L:
-            hup->hup_frame.data_len = hup->hup_frame.data_len | data_byte;
-            hup->state              = EM_HUP_STATE_DATA;
-            hup->xor8               = hup->xor8 ^ data_byte;
+            hup_ptr->hup_frame.data_len_l = data_byte;
+            hup_ptr->frame_data_len       = hup_ptr->frame_data_len | data_byte;
+            hup_ptr->state                = EM_HUP_STATE_DATA;
+            hup_ptr->xor8                 = hup_ptr->xor8 ^ data_byte;
             break;
 
         case EM_HUP_STATE_DATA:
-            hup->hup_frame.data_addr[hup->read_data_len++] = data_byte;
-            hup->xor8                                      = hup->xor8 ^ data_byte;
-            if (hup->read_data_len == hup->hup_frame.data_len) {
-                hup->state = EM_HUP_STATE_CRC;
+            hup_ptr->hup_frame.data_addr[hup_ptr->read_data_len++] = data_byte;
+            hup_ptr->xor8                                          = hup_ptr->xor8 ^ data_byte;
+            if (hup_ptr->read_data_len == hup_ptr->frame_data_len) {
+                hup_ptr->state = EM_HUP_STATE_CRC;
             }
             break;
 
         case EM_HUP_STATE_CRC:
-            hup->hup_frame.xor8 = data_byte;
-            hup->state          = EM_HUP_STATE_HDR_H;
-            if (hup->hup_frame.xor8 == hup->xor8) {
-                hup->hup_success_handle_func(hup->hup_frame);
+            hup_ptr->hup_frame.xor8 = data_byte;
+            hup_ptr->state          = EM_HUP_STATE_HDR_H;
+            if (hup_ptr->hup_frame.xor8 == hup_ptr->xor8) {
+                hup_ptr->hup_success_handle_cb(hup_ptr->hup_frame);
             }
             break;
 
--- applications/HL_A6902/utility/hup/hl_util_hup.h
+++ applications/HL_A6902/utility/hup/hl_util_hup.h
@@ -17,10 +17,36 @@
 /* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __HL_UTIL_HUP_H__
 #define __HL_UTIL_HUP_H__
+
 /* Includes ------------------------------------------------------------------*/
 #include <stdint.h>
 #include <stdio.h>
+#include <rtthread.h>
+
 /* typedef -------------------------------------------------------------------*/
+typedef enum
+{
+    EM_HUP_STATE_HDR_H,
+    EM_HUP_STATE_HDR_L,
+    EM_HUP_STATE_CMD,
+    EM_HUP_STATE_DATA_LEN_H,
+    EM_HUP_STATE_DATA_LEN_L,
+    EM_HUP_STATE_DATA,
+    EM_HUP_STATE_CRC,
+} hup_protocol_state_em;
+
+typedef enum
+{
+    EM_HUP_ROLE_MASTER = 0x11,
+    EM_HUP_ROLE_SLAVE,
+} hup_role_em;
+
+typedef enum
+{
+    EM_HUP_TIMER_ENABLE = 0x21,
+    EM_HUP_TIMER_DISABLE,
+} hup_timer_state_em;
+
 typedef struct
 {
     /// 协议头部高字节
@@ -29,27 +55,35 @@ typedef struct
     uint8_t hdr_l;
     /// 命令控制位
     uint8_t cmd;
-    /// 数据内容长度
-    uint16_t data_len;
+    /// 数据内容长度高字节
+    uint8_t data_len_h;
+    /// 数据内容长度低字节
+    uint8_t data_len_l;
     /// 数据内容地址
     uint8_t* data_addr;
     /// 异或校验位
     uint8_t xor8;
-} hup_protocol_type_t, *hup_protocol_type_p;
+} hup_protocol_type_t;
 
 typedef struct
 {
     /// 协议角色
-    uint8_t role;
-    /// 状态机当前状态
-    uint8_t state;
+    hup_role_em role;
+    /// 定时器使能状态
+    hup_timer_state_em timer_state;
+    /// 已申请存放数据缓冲区长度
+    uint16_t frame_data_len;
+} hup_handle_type_t;
+
+typedef struct
+{
     /// 异或校验值
     uint8_t xor8;
-    /// 定时器使能状态
-    uint8_t timer_state;
-    /// 缓冲区可存的长度
-    uint16_t buf_len;
-    /// 已解析数据的长度
+    /// 状态机当前状态
+    uint8_t state;
+    /// 记录当前帧的数据长度
+    uint16_t frame_data_len;
+    /// 记录当前已解析的数据长度
     uint16_t read_data_len;
     /// 上次时间戳值
     uint32_t last_timeout_val;
@@ -57,6 +91,8 @@ typedef struct
     uint32_t cur_timeout_val;
     /// 最大超时时间
     uint32_t max_timeout;
+    /// hup句柄结构体
+    hup_handle_type_t hup_handle;
     /// hup协议帧结构体
     hup_protocol_type_t hup_frame;
     /**
@@ -75,7 +111,7 @@ typedef struct
      */
     uint32_t (*get_time_stamp)(void);
     /**
-     * hup_success_handle_func
+     * hup_success_handle_cb
      * @brief hup解析成功后的处理函数
      * @date 2022-08-09
      * @author lisonglin (songlin.li@hollyland-tech.com)
@@ -88,53 +124,29 @@ typedef struct
      * <tr><td>2022-08-09      <td>lisonglin     <td>新建
      * </table>
      */
-    void (*hup_success_handle_func)(hup_protocol_type_t);
-} hup_protocol_handle_t, *hup_protocol_handle_p;
-
-typedef enum
-{
-    EM_HUP_STATE_HDR_H,
-    EM_HUP_STATE_HDR_L,
-    EM_HUP_STATE_CMD,
-    EM_HUP_STATE_DATA_LEN_H,
-    EM_HUP_STATE_DATA_LEN_L,
-    EM_HUP_STATE_DATA,
-    EM_HUP_STATE_CRC,
-} hup_protocol_state_em;
-
-typedef enum
-{
-    EM_HUP_ROLE_MASTER,
-    EM_HUP_ROLE_SLAVE,
-} hup_role_em;
-
-typedef enum
-{
-    EM_HUP_ENABLE,
-    EM_HUP_DISABLE,
-} hup_timer_state_em;
+    void (*hup_success_handle_cb)(hup_protocol_type_t);
+} hl_util_hup_t;
 
 /* define --------------------------------------------------------------------*/
-#define HL_UTIL_HUP_DBG printf
+#define HL_UTIL_HUP_DBG rt_kprintf
 #define HUP_MAX_TIMEOUT 10000
 
 #define HUP_FRAME_HOST_HDR_H 0xAA
 #define HUP_FRAME_HOST_HDR_L 0xDD
 #define HUP_FRAME_CLIENT_HDR_H 0xBB
 #define HUP_FRAME_CLIENT_HDR_L 0xDD
+
 /* variables -----------------------------------------------------------------*/
 /* Private function(only *.c)  -----------------------------------------------*/
 /**
- * hl_util_hup_protocol_init
+ * hl_util_hup_init
  * @brief 初始化hup协议帧结构体，申请内存空间，注册回调函数
- * @param [in] role hup当前角色
- * @param [in] timer_state 定时器使能状态
- * @param [in] buf_addr 缓冲区地址
- * @param [in] buf_size 缓冲区大小
+ * @param [in] hup_ptr hup结构体指针(传入该参数前，需对成员变量hup_handle结构体赋初值)
+ * @param [in] frame_data_addr 协议帧数据存放缓冲区
  * @param [in] get_time_stamp 获取当前时间戳（函数指针）
- * @param [in] hup_success_handle_func 解析成功后的处理（函数指针）
- * @return hup_protocol_handle_p 成功 返回hup句柄 | 失败 NULL
- * @date 2022-08-09
+ * @param [in] hup_success_handle_cb 解析成功后的处理（函数指针）
+ * @return int 成功 0 | 失败 -1
+ * @date 2022-09-13
  * @author lisonglin (songlin.li@hollyland-tech.com)
  * 
  * @details 
@@ -142,17 +154,16 @@ typedef enum
  * @par 修改日志:
  * <table>
  * <tr><th>Date             <th>Author         <th>Description
- * <tr><td>2022-08-09      <td>lisonglin     <td>新建
+ * <tr><td>2022-09-13      <td>lisonglin     <td>新建
  * </table>
  */
-hup_protocol_handle_p hl_util_hup_protocol_init(hup_role_em role, hup_timer_state_em timer_state, uint8_t* buf_addr,
-                                                uint16_t buf_size, uint32_t (*get_time_stamp)(void),
-                                                void (*hup_success_handle_func)(hup_protocol_type_t));
+int hl_util_hup_init(hl_util_hup_t* hup_ptr, uint8_t* frame_data_addr, uint32_t (*get_time_stamp)(void),
+                     void (*hup_success_handle_cb)(hup_protocol_type_t));
 
 /**
- * hl_util_hup_protocol_deinit
+ * hl_util_hup_deinit
  * @brief hup协议帧结构体去初始化，释放内存空间
- * @param [in] hup hup协议帧结构体
+ * @param [in] hup_ptr hup结构体指针
  * @return int 成功 0 | 失败 -1
  * @date 2022-08-09
  * @author lisonglin (songlin.li@hollyland-tech.com)
@@ -165,10 +176,10 @@ hup_protocol_handle_p hl_util_hup_protocol_init(hup_role_em role, hup_timer_stat
  * <tr><td>2022-08-09      <td>lisonglin     <td>新建
  * </table>
  */
-int hl_util_hup_protocol_deinit(hup_protocol_handle_p hup);
+int hl_util_hup_deinit(hl_util_hup_t* hup_ptr);
 
 /**
- * hl_util_hup_protocol_encode
+ * hl_util_hup_encode
  * @brief hup协议封包函数
  * @param [in] role hup当前角色
  * @param [in] cmd 控制命令
@@ -188,13 +199,13 @@ int hl_util_hup_protocol_deinit(hup_protocol_handle_p hup);
  * <tr><td>2022-08-09      <td>lisonglin     <td>新建
  * </table>
  */
-int hl_util_hup_protocol_encode(uint8_t role, uint8_t cmd, uint8_t* frame_buf, uint16_t buf_len, uint8_t* data_addr,
-                                uint16_t data_len);
+int hl_util_hup_encode(hup_role_em role, uint8_t cmd, uint8_t* frame_buf, uint16_t buf_len, uint8_t* data_addr,
+                       uint16_t data_len);
 
 /**
- * hl_util_hup_protocol_decode
+ * hl_util_hup_decode
  * @brief hup协议解包函数
- * @param [in] hup hup句柄
+ * @param [in] hup_ptr hup结构体指针
  * @param [in] data_byte 解析单个字节
  * @return int 成功 0 | 失败 -1
  * @date 2022-08-09
@@ -208,7 +219,7 @@ int hl_util_hup_protocol_encode(uint8_t role, uint8_t cmd, uint8_t* frame_buf, u
  * <tr><td>2022-08-09      <td>lisonglin     <td>新建
  * </table>
  */
-int hl_util_hup_protocol_decode(hup_protocol_handle_p hup, uint8_t data_byte);
+int hl_util_hup_decode(hl_util_hup_t* hup_ptr, uint8_t data_byte);
 
 #endif
 
