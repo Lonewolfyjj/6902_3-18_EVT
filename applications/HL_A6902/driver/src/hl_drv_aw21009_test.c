680f414214b5b4dfee208135f65f93cded878c82
lilin <lin.li@hollyland-tech.com>
2022-09-26 09:20:16 +0000

    ✨ 修改代码（Driver）：修改驱动代码   1、aw21009驱动测试函数增加一个设备编号参数 结束

--- applications/HL_A6902/driver/src/hl_drv_aw21009_test.c
+++ applications/HL_A6902/driver/src/hl_drv_aw21009_test.c
@@ -42,11 +42,11 @@ int hl_drv_aw21009_test_open(int argc, char* argv[])
 
     flag = true;
 
-    hl_drv_aw21009_ctrl(HL_DRV_AW21009_LED_DEV_1, HL_DRV_AW21009_SET_GROUP_CTRL_DISABLE, &flag, sizeof(flag));
+    hl_drv_aw21009_ctrl(atoi(argv[6]), HL_DRV_AW21009_SET_GROUP_CTRL_DISABLE, &flag, sizeof(flag));
 
     led_group = HL_DRV_AW21009_LED_GROUP_1 | HL_DRV_AW21009_LED_GROUP_2 | HL_DRV_AW21009_LED_GROUP_3;
 
-    hl_drv_aw21009_ctrl(HL_DRV_AW21009_LED_DEV_1, HL_DRV_AW21009_SET_LED_GROUP_MODE, &led_group, sizeof(led_group));
+    hl_drv_aw21009_ctrl(atoi(argv[6]), HL_DRV_AW21009_SET_LED_GROUP_MODE, &led_group, sizeof(led_group));
 
     auto_breath.rise_time    = 9;
     auto_breath.on_time      = 3;
@@ -56,7 +56,7 @@ int hl_drv_aw21009_test_open(int argc, char* argv[])
     auto_breath.start_point  = HL_DRV_AW21009_START_POINT_T0;
     auto_breath.stop_point   = HL_DRV_AW21009_STOP_POINT_T1;
 
-    hl_drv_aw21009_ctrl(HL_DRV_AW21009_LED_DEV_1, HL_DRV_AW21009_SET_AUTO_BREATH_PARAM, &auto_breath,
+    hl_drv_aw21009_ctrl(atoi(argv[6]), HL_DRV_AW21009_SET_AUTO_BREATH_PARAM, &auto_breath,
                         sizeof(auto_breath));
 
     light.r          = atoi(argv[1]);
@@ -65,11 +65,11 @@ int hl_drv_aw21009_test_open(int argc, char* argv[])
     light.brightness = atoi(argv[4]);
     light.led_group  = atoi(argv[5]);
 
-    hl_drv_aw21009_ctrl(HL_DRV_AW21009_LED_DEV_1, HL_DRV_AW21009_SET_LED_LIGHT_EFFECT, &light, sizeof(light));
+    hl_drv_aw21009_ctrl(atoi(argv[6]), HL_DRV_AW21009_SET_LED_LIGHT_EFFECT, &light, sizeof(light));
 
     pat_mode = HL_DRV_AW21009_PAT_AUTO_BREATH_MODE;
 
-    hl_drv_aw21009_ctrl(HL_DRV_AW21009_LED_DEV_1, HL_DRV_AW21009_SET_PATTERN_MODE, &pat_mode, sizeof(pat_mode));
+    hl_drv_aw21009_ctrl(atoi(argv[6]), HL_DRV_AW21009_SET_PATTERN_MODE, &pat_mode, sizeof(pat_mode));
 
     return 0;
 }
