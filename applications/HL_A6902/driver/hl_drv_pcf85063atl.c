4e3d9ded0d2848f0f44ad4a07c7e5d20bc9fd364
libo <rd46@hollyland-tech.com>
2022-10-14 15:53:03 +0800

    🐛 修复bug（Driver）：修复驱动代码 1.增加bcd码和10进制的互转 结束

--- applications/HL_A6902/driver/hl_drv_pcf85063atl.c
+++ applications/HL_A6902/driver/hl_drv_pcf85063atl.c
@@ -67,7 +67,7 @@ static rt_err_t write_reg(struct rt_i2c_bus_device* bus, rt_uint8_t reg, rt_uint
 
     /* 调用I2C设备接口传输数据 */
     if (rt_i2c_transfer(bus, &msgs, 1) == 1) {
-        rt_kprintf("RTC_PCF85063ATL  write_reg  ok\n");
+        //rt_kprintf("RTC_PCF85063ATL  write_reg  ok\n");
         return RT_EOK;
     } else {
         rt_kprintf("RTC_PCF85063ATL  write_reg  error %d\n", rt_i2c_transfer(bus, &msgs, 1));
@@ -104,7 +104,7 @@ static rt_err_t read_regs(struct rt_i2c_bus_device* bus, rt_uint8_t* buf, rt_uin
 
     /* 调用I2C设备接口传输数据 */
     if (rt_i2c_transfer(bus, &msgs, 1) == 1) {
-        rt_kprintf("RTC_PCF85063ATL  read_regs  ok\n");
+        //rt_kprintf("RTC_PCF85063ATL  read_regs  ok\n");
         return RT_EOK;
     } else {
         rt_kprintf("RTC_PCF85063ATL  read_regs  error\n");
@@ -112,6 +112,58 @@ static rt_err_t read_regs(struct rt_i2c_bus_device* bus, rt_uint8_t* buf, rt_uin
     }
 }
 
+/**
+ * 
+ * @brief 10进制转16进制 （只改变进制 不变数值）
+ * @param [in] data 
+ * @return u_int8_t 
+ * @date 2022-10-14
+ * @author libo (rd46@hollyland-tech.com)
+ * 
+ * @details 
+ * @note 
+ * @par 修改日志:
+ * <table>
+ * <tr><th>Date             <th>Author         <th>Description
+ * <tr><td>2022-10-14      <td>libo     <td>新建
+ * </table>
+ */
+static uint8_t DecimalToBCD(uint8_t data)
+{
+    uint8_t high = 0;
+
+    while(data >= 10){
+        high++;
+        data -= 10;
+    }
+
+    return((high<<4)|data);
+}
+
+/**
+ * 
+ * @brief 16进制转10进制 （只改变进制 不变数值）
+ * @param [in] data 
+ * @return u_int8_t 
+ * @date 2022-10-14
+ * @author libo (rd46@hollyland-tech.com)
+ * 
+ * @details 
+ * @note 
+ * @par 修改日志:
+ * <table>
+ * <tr><th>Date             <th>Author         <th>Description
+ * <tr><td>2022-10-14      <td>libo     <td>新建
+ * </table>
+ */
+static uint8_t BCDToDecimal(uint8_t data)
+{
+    uint8_t tenweight,oneweight,result;
+    oneweight =  data&0x0f;
+    tenweight = (data&0xf0)>>4;
+    result = tenweight*10 + oneweight*1;
+    return result;
+}
 
 uint8_t hl_drv_rtc_pcf85063_init()
 {
@@ -187,7 +239,7 @@ static uint8_t hl_get_rtc_time(void* ptr)
  */
 static uint8_t hl_lock_rtc_clock(void)
 {
-    rt_uint8_t reg0x0;
+    rt_uint8_t reg0x0 = 0x00;
     if (write_reg(i2c_handle, RTC_PCF85063_Control_1, NULL, 0)) {
         rt_kprintf("RTC_PCF85063ATL  write_reg  RTC_PCF85063_Control_1 fail\n");
         return -1;
@@ -197,12 +249,13 @@ static uint8_t hl_lock_rtc_clock(void)
         rt_kprintf("RTC_PCF85063ATL  read_regs  RTC_PCF85063_Control_1 fail\n");
         return -1;
     }
-
     reg0x0 |=0x20;
+
     if (write_reg(i2c_handle, RTC_PCF85063_Control_1, &reg0x0, 1)) {
         rt_kprintf("RTC_PCF85063ATL  write_reg RTC_PCF85063_Control_1  fail\n");
         return -1;
     }
+
 
     return 0;
 
@@ -225,7 +278,7 @@ static uint8_t hl_lock_rtc_clock(void)
  */
 static uint8_t hl_unlock_rtc_clock(void)
 {
-    rt_uint8_t reg0x0;
+    rt_uint8_t reg0x0 = 0x00;
     if (write_reg(i2c_handle, RTC_PCF85063_Control_1, NULL, 0)) {
         rt_kprintf("RTC_PCF85063ATL  write_reg RTC_PCF85063_Control_1  fail\n");
         return -1;
@@ -241,6 +294,7 @@ static uint8_t hl_unlock_rtc_clock(void)
         rt_kprintf("RTC_PCF85063ATL  write_reg RTC_PCF85063_Control_1  fail\n");
         return -1;
     }
+
 
     return 0;
 }
@@ -294,6 +348,13 @@ uint8_t hl_drv_rtc_pcf85063_io_ctrl(uint8_t cmd, void* ptr, uint16_t len)
         return 1;
     }
 
+    uint8_t* ptr1 = (uint8_t*) ptr;
+    
+    /*将输入参数10进制转换成bcd码*/
+    for (size_t i = 0; i < len; i++){
+        ptr1[i]= DecimalToBCD(ptr1[i]);
+    }
+    
     switch (cmd) {
         case RTC_GET_TIME:
             if (hl_get_rtc_time(ptr)) {
@@ -309,6 +370,12 @@ uint8_t hl_drv_rtc_pcf85063_io_ctrl(uint8_t cmd, void* ptr, uint16_t len)
             break;
         default:
             break;
+    }
+
+    
+    /*将输出参数bcd码转换成10进制*/
+    for (size_t i = 0; i < len; i++){
+         ptr1[i]= BCDToDecimal(ptr1[i]);
     }
 
     return 0;
--- applications/HL_A6902/driver/hl_drv_pcf85063atl_test.c
+++ applications/HL_A6902/driver/hl_drv_pcf85063atl_test.c
@@ -28,6 +28,8 @@
 #include <rtdevice.h>
 #include"hl_drv_pcf85063atl.h"
 
+
+
 /**
  * 
  * @brief demo
@@ -51,14 +53,15 @@ void hl_drv_pcf85063atl_export(int argc, char** argv)
 
     hl_drv_rtc_pcf85063_init();
     rt_kprintf(" argc :%d argv[0]: %s\n", argc, argv[1]);
-    if (argc == 7) {
+    if (argc == 8) {
         if (!strcmp("set", argv[1])) {
             time.year   = atoi(argv[2]);
             time.month  = atoi(argv[3]);
             time.day    = atoi(argv[4]);
             time.hour   = atoi(argv[5]);
             time.minute = atoi(argv[6]);
-            hl_drv_rtc_pcf85063_io_ctrl(RTC_SET_TIME, (void*)&time, 0);
+            time.second = atoi(argv[7]);
+            hl_drv_rtc_pcf85063_io_ctrl(RTC_SET_TIME, (void*)&time, sizeof(rtc_time));
         } else {
             rt_kprintf(" parm1 no set!\n");
         }
@@ -66,12 +69,14 @@ void hl_drv_pcf85063atl_export(int argc, char** argv)
 
     if (argc == 2) {
         if (!strcmp("get", argv[1])) {
-            hl_drv_rtc_pcf85063_io_ctrl(RTC_GET_TIME, (void*)&time, 0);
-            rt_kprintf(" year: %d\n", time.year);
-            rt_kprintf(" month: %d\n", time.month);
-            rt_kprintf(" day: %d\n", time.day);
-            rt_kprintf(" hour: %d\n", time.hour);
-            rt_kprintf(" minute: %d\n", time.minute);
+            hl_drv_rtc_pcf85063_io_ctrl(RTC_GET_TIME, (void*)&time, sizeof(rtc_time));
+            /*时间存储格式是按 BCD 码的格式来存储的。如果需要使用，需要转换成十六进制之后才能使用，时间年的范围：0~99，需要注意*/
+            rt_kprintf(" year:   %d\n", time.year);
+            rt_kprintf(" month:  %d\n", time.month&0x1f);
+            rt_kprintf(" day:    %d\n", time.day&0x3f);
+            rt_kprintf(" hour:   %d\n", time.hour&0x3f);
+            rt_kprintf(" minute: %d\n", time.minute&0x7f);
+            rt_kprintf(" second: %d\n", time.second&0x7f);
         } else {
             rt_kprintf(" parm1 no  get!\n");
         }
