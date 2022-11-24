198e5ce1d802941cad4d28275db36e7b3ec42fee
luzhanghao <rd14@hollyland-tech.com>
2022-11-24 13:17:23 +0000

    ✅ 测试通过(hid):添加hid读，优化hid写   1、将hid数据存在ringbuff中，read时读出   2、优化hid写，数据存放在链表中，可以连续write 结束

--- applications/usb_app/test_usb.c
+++ applications/usb_app/test_usb.c
@@ -5,160 +5,73 @@
 #include <stdio.h>
 #include "../../components/drivers/usb/usbdevice/class/hid.h"
 
-#define KEYBOARD_A 0x4
-#define KEYBOARD_B 0x5
+ALIGN(4) rt_uint8_t report_send_buf[70];
+ALIGN(4) rt_uint8_t report_receive_buf[64];
+static rt_thread_t hid_thread = RT_NULL;
 
-struct keyboard
+static void hid_thread_entry(void *parameter)
 {
-    char key_value[7];
-} keyboard;
-
-static int keyboard_send_key_release(rt_device_t device)
-{
-    int i;
-
-    RT_ASSERT(device != RT_NULL);
-
-    /* init input date as key_release */
-    for (i = 0; i < 7; i++)
-        keyboard.key_value[i] = 0;
-
-    rt_device_write(device, 0, keyboard.key_value, 7);
-    return 0;
-}
-
-static int keyboard_send_key_value(rt_device_t device, int value)
-{
-    rt_uint8_t report_id;
-    RT_ASSERT(device != RT_NULL);
-    keyboard.key_value[1] = value;
-    report_id = 0x00;
-
-    rt_device_write(device, report_id, keyboard.key_value, 7);
-    return 0;
-}
-
-static void usb_send_data(rt_device_t device)
-{
-    RT_ASSERT(device != RT_NULL);
-#if 0
-    rt_kprintf("start send hid keyboard data\n");
-    /* set key value */
-    keyboard_send_key_value(device, KEYBOARD_A);
-    rt_thread_mdelay(10);
-    keyboard_send_key_release(device);
-    rt_thread_mdelay(10);
-    keyboard_send_key_value(device, KEYBOARD_B);
-    rt_thread_mdelay(10);
-    keyboard_send_key_release(device);
-    rt_thread_mdelay(10);
-    rt_kprintf("complete send hid keyboard data\n");
-#else 
-    rt_uint8_t report_id;
-    char key_value[7];
-    report_id = 0x05;
-    key_value[0] = 0x11;
-    key_value[1] = 0x22;
-    key_value[2] = 0x33;
-    key_value[3] = 0x44;
-    key_value[4] = 0x55;
-    key_value[5] = 0x66;
-    key_value[6] = 0x77;
-    rt_device_write(device, report_id, key_value, 7);
-    // rt_thread_mdelay(10);
-    // rt_device_write(device, report_id, key_value, 1);
-    // rt_thread_mdelay(10);
-    // rt_device_write(device, report_id, key_value, 1);
-    rt_thread_mdelay(200);
-
-    key_value[0] = 0x01;
-    key_value[1] = 0x02;
-    key_value[2] = 0x03;
-    key_value[3] = 0x04;
-    key_value[4] = 0x05;
-    key_value[5] = 0x06;
-    key_value[6] = 0x07;
-    rt_device_write(device, report_id, key_value, 7);
-    rt_thread_mdelay(10);
-    // rt_device_write(device, report_id, key_value, 1);
-    // rt_thread_mdelay(10);
-    // rt_device_write(device, report_id, key_value, 1);
-    // rt_thread_mdelay(10);
-
-#endif
-}
-
-ALIGN(4) rt_uint8_t report_buf[64];
-static void usb_thread_entry(void *parameter)
-{
-    rt_device_t device = (rt_device_t)parameter;
-    struct urequest setup;
-    int i;
-
-    usb_send_data(device);
-#if 0
-    while (1)
-    {
-        rt_memset(&setup, 0, sizeof(setup));
-        rt_memset(report_buf, 0, 64);
-        rt_device_control(device, RK_USB_HID_SETUP_READ, &setup);
-        rt_kprintf("setup ");
-        for(i = 0; i < 8; i++)
-                rt_kprintf("%02x", *(((rt_uint8_t*)&setup) + i));
-        rt_kprintf("\n");
-        switch (setup.bRequest)
-        {
-        case USB_HID_REQ_SET_REPORT:
-            rt_device_control(device, RK_USB_HID_REPORT_DATA_READ, report_buf);
-            rt_kprintf("report data ");
-            for(i = 0; i < setup.wLength; i++)
-                rt_kprintf("%02x", *(((rt_uint8_t*)report_buf) + i));
-            rt_kprintf("\n");
-            break;
-        case USB_HID_REQ_GET_REPORT:
-            rt_device_control(device, RK_USB_HID_REPORT_DATA_WRITE, report_buf);
-            break;
-        default:
-            break;
-        }
-    }
-#endif
-    rt_device_close(device);
-}
-
-static int hid_test(void)
-{
-    static rt_thread_t hid_thread = RT_NULL;
+    rt_uint8_t  size,i;
     /* find hidd device */
     rt_device_t device = rt_device_find("hidd");
 
     if (device == RT_NULL) {
         rt_kprintf("err:no find hidd\r\n");
+        hid_thread = RT_NULL;
         return -1;
     }
 
     /* open hidd device */
-    rt_device_open(device, RT_DEVICE_FLAG_WRONLY);
+    rt_device_open(device, RT_DEVICE_FLAG_RDWR);
 
-    /* create hid dynamic thread */
-    hid_thread = rt_thread_create("hid_test", usb_thread_entry, device,
-                                  1024, 10, 20);
-    rt_thread_startup(hid_thread);
-    return 0;
+    for (i = 0; i < 70; i++) {
+        report_send_buf[i] = i;
+    }
+    rt_device_write(device, 0, report_send_buf, 70);
+    rt_device_write(device, 0, report_send_buf, 20);
+    while (1) {
+        size = rt_device_read(device, 0, report_receive_buf, 64);
+        if (size > 0) {
+            rt_kprintf("hid receive:\r\n");
+            for (i = 0; i < size; i++) {
+                rt_kprintf("0x%02x ", report_receive_buf[i]);
+                if (i%8 == 7) {
+                    rt_kprintf("\r\n");
+                }
+            }
+        }
+        rt_thread_mdelay(10);
+    }
+
+    rt_device_close(device);
 }
 
 // hid_send_test [report_id] [key_value] 
-int hid_send_test(int argc, char** argv)
+int hid_key_test(int argc, char** argv)
 {
-    rt_thread_t hid_thread;
     rt_uint8_t report_id;
-    char key_value[7];
+    rt_uint8_t key_value;
+    rt_uint8_t buff[2] = {0};
+
+    rt_uint8_t i;
+    rt_uint8_t len;
+    char *endptr;
 
     if (argc != 3) {
         rt_kprintf("wrong parameter, please type: hid_send_test [report_id] [key_value]  \r\n");
         return -1;
     }
 
+    for (i = 0; i < (argc - 1); i++) {
+        buff[i] = strtoul(argv[i+1], &endptr, 0);
+        if (*endptr) {
+            rt_kprintf("Bad <addr> value '%s'\n", argv[i]);
+            return 0;
+        }
+    }
+    report_id = buff[0];
+    key_value = buff[1];
+
     /* find hidd device */
     rt_device_t device = rt_device_find("hidd");
 
@@ -170,18 +83,86 @@ int hid_send_test(int argc, char** argv)
     /* open hidd device */
     rt_device_open(device, RT_DEVICE_FLAG_WRONLY);
 
-    report_id    = atoi(argv[1]);
-    key_value[0] = atoi(argv[2]);
-    rt_device_write(device, report_id, key_value, 1);
-    rt_thread_mdelay(10);
+    // 发送按键按下
+    rt_device_write(device, 0, buff, 2);
+    // rt_thread_mdelay(10);
+    // rt_kprintf("hid key send 0x%x 0x%x\n", report_id, key_value);
     
-    key_value[0] = 0x00;
-    rt_device_write(device, report_id, key_value, 1);
-    rt_thread_mdelay(10);
+    // 发送按键释放
+    buff[1] = 0x00;
+    rt_device_write(device, 0, buff, 2);
+    // rt_thread_mdelay(10);
+    // rt_kprintf("hid key send 0x%x 0x%x\n", report_id, key_value);
 
     rt_device_close(device);
     return 0;
 }
 
-MSH_CMD_EXPORT(hid_test, hid in test)
+// hid_send_test data[0] data[1].. data[n]     n <= 64
+int hid_send_test(int argc, char** argv)
+{
+    rt_uint8_t i;
+    char *endptr;
+
+    if (argc <= 1) {
+        rt_kprintf("wrong parameter, please type: hid_send_test data[0] data[1].. data[n](n <= 64)\r\n");
+        return -1;
+    }
+
+    for (i = 0; i < (argc - 1); i++) {
+        report_send_buf[i] = strtoul(argv[i+1], &endptr, 0);
+        if (*endptr) {
+            rt_kprintf("Bad <addr> value '%s'\n", argv[i]);
+            return 0;
+        }
+    }
+
+    /* find hidd device */
+    rt_device_t device = rt_device_find("hidd");
+
+    if (device == RT_NULL) {
+        rt_kprintf("err:no find hidd\r\n");
+        return -1;
+    }
+
+    /* open hidd device */
+    rt_device_open(device, RT_DEVICE_FLAG_WRONLY);
+
+    // 发送按键按下
+    rt_device_write(device, report_send_buf[0], &report_send_buf[1], (argc - 2));
+    rt_thread_mdelay(10);
+    rt_kprintf("hid send data:\n");
+    for (i = 0; i < (argc - 1); i++) {
+        rt_kprintf("0x%x ", report_send_buf[i]);
+        if ((i%8) == 0) {
+            rt_kprintf("\n");
+        }
+    }
+    
+    rt_device_close(device);
+    return 0;
+}
+
+// hid_receive_test
+int hid_receive_test(int argc, char** argv)
+{
+    if (argc != 1) {
+        rt_kprintf("wrong parameter, please type: hid_receive_test \r\n");
+        return -1;
+    }
+
+    if (hid_thread != RT_NULL) {
+        rt_kprintf("hid_rec is ready\r\n");
+        return 0;
+    }
+
+    hid_thread = rt_thread_create("hid_rec", hid_thread_entry, RT_NULL, 1024, 20, 1);
+    if (hid_thread) {
+        rt_thread_startup(hid_thread);
+    }
+    return 0;
+}
+
+MSH_CMD_EXPORT(hid_key_test, hid in test)
 MSH_CMD_EXPORT(hid_send_test, hid send test)
+MSH_CMD_EXPORT(hid_receive_test, hid receive test)
