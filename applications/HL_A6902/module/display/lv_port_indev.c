e5f8e6711d1677aeb81f891cbcca8bfac9965af9
liujie <jie.liu@hollyland-tech.com>
2022-12-05 09:55:40 +0000

    🚧 格式化

--- applications/HL_A6902/module/display/lv_port_indev.c
+++ applications/HL_A6902/module/display/lv_port_indev.c
@@ -15,15 +15,12 @@
 #include "hl_drv_ztw523a.h"
 #endif
 
-static lv_group_t * key_group;
+static lv_group_t* key_group;
 
-
-
-
-static lv_indev_t * indev_touchpad_p;
-static lv_indev_t * indev_keypad_touchkey_p;
-static lv_indev_t * indev_keypad_knob_ok_p;
-static lv_indev_t * indev_encoder_knob_p;
+static lv_indev_t* indev_touchpad_p;
+static lv_indev_t* indev_keypad_touchkey_p;
+static lv_indev_t* indev_keypad_knob_ok_p;
+static lv_indev_t* indev_encoder_knob_p;
 
 static lv_indev_drv_t indev_touchpad;
 static lv_indev_drv_t indev_keypad_touchkey;
@@ -32,61 +29,49 @@ static lv_indev_drv_t indev_encoder_knob;
 
 // static hl_indev_s hl_indev_callback;
 
-
-
 static bool keypad_touchkey_switch = false;
-static bool touchpad_switch = false;
-
-
-
-
-
+static bool touchpad_switch        = false;
 
 // 触摸屏
 
 static void touchpad_init(void);
-static void touchpad_read(lv_indev_drv_t * indev_drv, lv_indev_data_t * data);
+static void touchpad_read(lv_indev_drv_t* indev_drv, lv_indev_data_t* data);
 static bool touchpad_is_pressed(void);
-static void touchpad_get_xy(lv_coord_t * x, lv_coord_t * y);
-
+static void touchpad_get_xy(lv_coord_t* x, lv_coord_t* y);
 
 // 返回触摸按键
 static void keypad_touchkey_init(void);
-static void keypad_touchkey_read(lv_indev_drv_t * indev_drv, lv_indev_data_t * data);
+static void keypad_touchkey_read(lv_indev_drv_t* indev_drv, lv_indev_data_t* data);
 // static uint32_t keypad_touchkey_key(void);
-
 
 //旋钮OK按键
 static void keypad_knob_ok_init(void);
-static void keypad_knob_ok_read(lv_indev_drv_t * indev_drv, lv_indev_data_t * data);
+static void keypad_knob_ok_read(lv_indev_drv_t* indev_drv, lv_indev_data_t* data);
 // static uint32_t keypad_keypad_knob_ok_key(void);
 
 //旋钮
 static void encoder_knob_init(void);
-static void encoder_knob_read(lv_indev_drv_t * indev_drv, lv_indev_data_t * data);
+static void encoder_knob_read(lv_indev_drv_t* indev_drv, lv_indev_data_t* data);
 static void encoder_knob_handler(void);
-
-
 
 // static void hl_lv_indev_read_reg(hl_indev_s hl_indev_callback);
 
-static void lv_add_all_indev_to_def_group(lv_group_t* group);
-static int32_t encoder_diff;
+static void             lv_add_all_indev_to_def_group(lv_group_t* group);
+static int32_t          encoder_diff;
 static lv_indev_state_t encoder_state;
-
 
 #ifdef RT_USING_FT3169
 static struct fts_ts_event touch_pos = {
     .type = 1,
-    .x = 0,
-    .y = 0,
+    .x    = 0,
+    .y    = 0,
 };
 #endif
 #ifdef RT_USING_ZTW523A
 static struct ztw523a_ts_event touch_pos = {
     .type = 1,
-    .x = 0,
-    .y = 0,
+    .x    = 0,
+    .y    = 0,
 };
 #endif
 
@@ -98,18 +83,18 @@ void lv_port_indev_init(void)
     touchpad_init();
     /*Register a touchpad input device*/
     lv_indev_drv_init(&indev_touchpad);
-    indev_touchpad.type = LV_INDEV_TYPE_POINTER;
+    indev_touchpad.type    = LV_INDEV_TYPE_POINTER;
     indev_touchpad.read_cb = touchpad_read;
-    indev_touchpad_p = lv_indev_drv_register(&indev_touchpad);
+    indev_touchpad_p       = lv_indev_drv_register(&indev_touchpad);
 #endif
 
     // keypad_knob_ok_callback = keypad_knob_ok_read;
     // static lv_indev_t * indev_keypad_knob_ok;
     keypad_knob_ok_init();
     lv_indev_drv_init(&indev_keypad_knob_ok);
-    indev_keypad_knob_ok.type = LV_INDEV_TYPE_KEYPAD;
+    indev_keypad_knob_ok.type    = LV_INDEV_TYPE_KEYPAD;
     indev_keypad_knob_ok.read_cb = keypad_knob_ok_read;
-    indev_keypad_knob_ok_p = lv_indev_drv_register(&indev_keypad_knob_ok);
+    indev_keypad_knob_ok_p       = lv_indev_drv_register(&indev_keypad_knob_ok);
 
     /*Later you should create group(s) with `lv_group_t * group = lv_group_create()`,
      *add objects to the group with `lv_group_add_obj(group, obj)`
@@ -118,15 +103,15 @@ void lv_port_indev_init(void)
 
     keypad_touchkey_init();
     lv_indev_drv_init(&indev_keypad_touchkey);
-    indev_keypad_touchkey.type = LV_INDEV_TYPE_KEYPAD;
+    indev_keypad_touchkey.type    = LV_INDEV_TYPE_KEYPAD;
     indev_keypad_touchkey.read_cb = keypad_touchkey_read;
-    indev_keypad_touchkey_p = lv_indev_drv_register(&indev_keypad_touchkey);
-    
+    indev_keypad_touchkey_p       = lv_indev_drv_register(&indev_keypad_touchkey);
+
     encoder_knob_init();
     lv_indev_drv_init(&indev_encoder_knob);
-    indev_encoder_knob.type = LV_INDEV_TYPE_ENCODER;
+    indev_encoder_knob.type    = LV_INDEV_TYPE_ENCODER;
     indev_encoder_knob.read_cb = encoder_knob_read;
-    indev_encoder_knob_p = lv_indev_drv_register(&indev_encoder_knob);
+    indev_encoder_knob_p       = lv_indev_drv_register(&indev_encoder_knob);
 
     lv_add_all_indev_to_def_group(NULL);
 }
@@ -137,14 +122,11 @@ void lv_port_indev_init(void)
 
 static void lv_add_all_indev_to_def_group(lv_group_t* group)
 {
-    if (!group)
-    {
+    if (!group) {
         group = lv_group_get_default();
-        if (!group)
-        {
+        if (!group) {
             group = lv_group_create();
-            if (group)
-            {
+            if (group) {
                 lv_group_set_default(group);
             }
         }
@@ -164,12 +146,10 @@ static void lv_add_all_indev_to_def_group(lv_group_t* group)
 static void touchpad_init(void)
 {
     touchpad_switch = true;
-
-
 }
 
 /*Will be called by the library to read the touchpad*/
-static void touchpad_read(lv_indev_drv_t * indev_drv, lv_indev_data_t * data)
+static void touchpad_read(lv_indev_drv_t* indev_drv, lv_indev_data_t* data)
 {
 
     static lv_coord_t last_x = 0;
@@ -195,26 +175,26 @@ static void touchpad_read(lv_indev_drv_t * indev_drv, lv_indev_data_t * data)
 /*Return true is the touchpad is pressed*/
 static bool touchpad_is_pressed(void)
 {
-    #ifdef RT_USING_FT3169
+#ifdef RT_USING_FT3169
     /*Your code comes here*/
     hl_drv_touch_dev_read_info(&touch_pos);
-    if(touch_pos.type != 1){
+    if (touch_pos.type != 1) {
         return true;
     }
     return false;
-    #endif
-    #ifdef RT_USING_ZTW523A
+#endif
+#ifdef RT_USING_ZTW523A
     /*Your code comes here*/
     hl_drv_ztw523a_dev_read_info(&touch_pos);
-    if(touch_pos.type != 1){
+    if (touch_pos.type != 1) {
         return true;
     }
     return false;
-    #endif
+#endif
 }
 
 /*Get the x and y coordinates if the touchpad is pressed*/
-static void touchpad_get_xy(lv_coord_t * x, lv_coord_t * y)
+static void touchpad_get_xy(lv_coord_t* x, lv_coord_t* y)
 {
     /*Your code comes here*/
     (*x) = touch_pos.x;
@@ -226,24 +206,23 @@ static void touchpad_get_xy(lv_coord_t * x, lv_coord_t * y)
  * -----------------*/
 static void keypad_knob_ok_init(void)
 {
-    
 }
 
-static void keypad_knob_ok_read(lv_indev_drv_t * indev_drv, lv_indev_data_t * data)
+static void keypad_knob_ok_read(lv_indev_drv_t* indev_drv, lv_indev_data_t* data)
 {
     hl_mod_rx_knob_key_pro(indev_drv, data);
 }
 void keypad_knob_ok_update(keypad_knob_ok_callback c)
 {
-    static lv_indev_drv_t new_drv; 
-    lv_indev_t * indev_p;
+    static lv_indev_drv_t new_drv;
+    lv_indev_t*           indev_p;
 
     lv_indev_drv_init(&new_drv);
-    new_drv.type = LV_INDEV_TYPE_KEYPAD;
+    new_drv.type    = LV_INDEV_TYPE_KEYPAD;
     new_drv.read_cb = c;
-    indev_p = lv_indev_drv_register(&new_drv);
+    indev_p         = lv_indev_drv_register(&new_drv);
 
-    lv_indev_drv_update(indev_keypad_knob_ok_p,&new_drv);
+    lv_indev_drv_update(indev_keypad_knob_ok_p, &new_drv);
     indev_keypad_knob_ok_p = indev_p;
 }
 
@@ -252,14 +231,14 @@ static void keypad_touchkey_init(void)
     keypad_touchkey_switch = true;
 }
 
-static void keypad_touchkey_read(lv_indev_drv_t * indev_drv, lv_indev_data_t * data)
+static void keypad_touchkey_read(lv_indev_drv_t* indev_drv, lv_indev_data_t* data)
 {
     static uint8_t last_key = 0;
 
     last_key = hl_drv_ztw523a_botton_status();
     if (last_key != BUTTON_UP) {
         data->state = LV_INDEV_STATE_PR;
-        last_key = LV_KEY_BACKSPACE;
+        last_key    = LV_KEY_BACKSPACE;
         LV_LOG_USER("touchkey_en");
     } else {
         data->state = LV_INDEV_STATE_REL;
@@ -270,14 +249,12 @@ static void keypad_touchkey_read(lv_indev_drv_t * indev_drv, lv_indev_data_t * d
  * Encoder
  * -----------------*/
 
-
 static void encoder_knob_init(void)
 {
     // 初始化了
 }
 
-
-static void encoder_knob_read(lv_indev_drv_t * indev_drv, lv_indev_data_t * data)
+static void encoder_knob_read(lv_indev_drv_t* indev_drv, lv_indev_data_t* data)
 {
 
     data->enc_diff = 0;
@@ -286,18 +263,16 @@ static void encoder_knob_read(lv_indev_drv_t * indev_drv, lv_indev_data_t * data
 
 void encode_knob_update(keypad_knob_ok_callback c)
 {
-    static lv_indev_drv_t new_drv; 
-    lv_indev_t * indev_p;
+    static lv_indev_drv_t new_drv;
+    lv_indev_t*           indev_p;
 
     lv_indev_drv_init(&new_drv);
-    new_drv.type = LV_INDEV_TYPE_ENCODER;
+    new_drv.type    = LV_INDEV_TYPE_ENCODER;
     new_drv.read_cb = c;
-    indev_p = lv_indev_drv_register(&new_drv);
+    indev_p         = lv_indev_drv_register(&new_drv);
 
-    lv_indev_drv_update(indev_encoder_knob_p,&new_drv);
+    lv_indev_drv_update(indev_encoder_knob_p, &new_drv);
     indev_encoder_knob_p = indev_p;
 }
 
-
 #endif /*!HL_IS_TX_DEVICE()*/
-
