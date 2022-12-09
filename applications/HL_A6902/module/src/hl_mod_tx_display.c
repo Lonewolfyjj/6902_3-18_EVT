97c1164b9583b22671fa8e8cd9d8ce9735c5b7c0
lilin <lin.li@hollyland-tech.com>
2022-12-09 08:16:10 +0000

    ✨ 完善功能（mod）：完善tx显示功能   1、完成tx显示模块源文件 结束

--- applications/HL_A6902/module/src/hl_mod_tx_display.c
+++ applications/HL_A6902/module/src/hl_mod_tx_display.c
@@ -1,27 +1,454 @@
-// #include <rtthread.h>
-// #include <rtdevice.h>
-// #include "lvgl.h"
-// #include "hl_mod_lvgl.h"
-// #include "lv_port_indev.h"
+/**
+ * @file hl_mod_tx_display.c
+ * @author lilin (lin.li@hollyland-tech.com)
+ * @brief 
+ * @version 1.0
+ * @date 2022-12-08
+ * 
+ * ██╗  ██╗ ██████╗ ██╗     ██╗  ██╗   ██╗██╗      █████╗ ███╗   ██╗██████╗ 
+ * ██║  ██║██╔═══██╗██║     ██║  ╚██╗ ██╔╝██║     ██╔══██╗████╗  ██║██╔══██╗
+ * ███████║██║   ██║██║     ██║   ╚████╔╝ ██║     ███████║██╔██╗ ██║██║  ██║
+ * ██╔══██║██║   ██║██║     ██║    ╚██╔╝  ██║     ██╔══██║██║╚██╗██║██║  ██║
+ * ██║  ██║╚██████╔╝███████╗███████╗██║   ███████╗██║  ██║██║ ╚████║██████╔╝
+ * ╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚══════╝╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝
+ * @copyright Copyright (c) 2022 hollyland
+ * 
+ * @par 修改日志:
+ * <table>
+ * <tr><th>Date           <th>Version  <th>Author         <th>Description
+ * <tr><td>2022-12-08     <td>v1.0     <td>lilin     <td>内容
+ * </table>
+ * 
+ */
+/* Define to prevent recursive inclusion -------------------------------------*/
+/* Includes ------------------------------------------------------------------*/
 
-// static int hl_mod_lvgl_init(void)
-// {
-//     uint8_t i = 0;
-//     rt_kprintf("lvgl_init \n");
-//     lv_init();
-//     // lv_port_disp_init();
-//     lv_port_indev_init();
-//     // lv_user_gui_init();
+#include "hl_mod_display.h"
+#include <stdbool.h>
 
-//     /* handle the tasks of LVGL */
-//     while(i<20)
-//     {
-//         i++;
-//         rt_kprintf("lvgl_init : LV_COLOR_MIX_ROUND_OFS = %d\n",LV_COLOR_MIX_ROUND_OFS);
-//         lv_task_handler();
-//         rt_thread_mdelay(LV_DISP_DEF_REFR_PERIOD);
-//     }
-//     return 0;
-// }
+#include "hl_drv_aw2016a.h"
 
-// MSH_CMD_EXPORT(hl_mod_lvgl_init, run lvgl_mod_init);
+#define DBG_SECTION_NAME "display"
+#define DBG_LEVEL DBG_INFO
+#include "rtdbg.h"
+
+#if HL_IS_TX_DEVICE()
+// TX
+
+/* typedef -------------------------------------------------------------------*/
+
+typedef enum _hl_mod_display_state_e
+{
+    HL_DISPLAY_IDLE,
+    HL_DISPLAY_CHARGING,
+    HL_DISPLAY_CHARGE_FULL,
+    HL_DISPLAY_NORMAL_SOC,
+    HL_DISPLAY_FAULT,
+    HL_DISPLAY_UPDATE,
+    HL_DISPLAY_PAIR,
+    HL_DISPLAY_CONNECTED_DENOISE,
+    HL_DISPLAY_RECONNECT_DENOISE,
+    HL_DISPLAY_CONNECTED,
+    HL_DISPLAY_RECONNECT,
+    HL_DISPLAY_LOWPOWER,
+    HL_DISPLAY_MUTE,
+    HL_DISPLAY_RECORD,
+    HL_DISPLAY_RECORD_STOP,
+    HL_DISPLAY_RECORD_ERR,
+} hl_mod_display_state_e;
+
+typedef struct _hl_mod_display_st
+{
+    bool                   init_flag;
+    bool                   aw2016a_init_flag;
+    bool                   display_update_flag_1;
+    bool                   display_update_flag_2;
+    uint8_t                charging_led_count;
+    hl_mod_display_state_e display_state_1;
+    hl_mod_display_state_e display_state_2;
+    hl_led_net_mode        net_mode;
+    hl_led_switch          record_state;
+    hl_led_switch          record_fault_state;
+    hl_led_switch          denoise_state;
+    hl_charge_mode         charge_state;
+    hl_led_switch          fault_state;
+    hl_led_switch          mute_state;
+    hl_led_switch          update_state;
+    uint8_t                bat_soc;
+    uint8_t                led_brightness;
+    void*                  msg_hd;
+    rt_thread_t            display_thread;
+    int                    thread_exit_flag;
+} hl_mod_display_st;
+
+/* define --------------------------------------------------------------------*/
+/* variables -----------------------------------------------------------------*/
+
+static hl_mod_display_st _display_mod = {
+    .init_flag         = false,
+    .aw2016a_init_flag = false,
+    .msg_hd            = NULL,
+};
+
+/* Private function(only *.c)  -----------------------------------------------*/
+
+static void _display_state_check_1(void)
+{
+    hl_mod_display_state_e state;
+
+    if (_display_mod.charging_led_count != 0) {
+        state = HL_DISPLAY_CHARGING;
+        _display_mod.charging_led_count--;
+    } else if (_display_mod.fault_state == SWITCH_OPEN) {
+        state = HL_DISPLAY_FAULT;
+    } else if (_display_mod.update_state == SWITCH_OPEN) {
+        state = HL_DISPLAY_UPDATE;
+    } else if (_display_mod.net_mode == LED_NET_MODE_PAIR) {
+        state = HL_DISPLAY_PAIR;
+    } else if (_display_mod.bat_soc <= 6) {
+        state = HL_DISPLAY_LOWPOWER;
+    } else if (_display_mod.mute_state == SWITCH_OPEN) {
+        state = HL_DISPLAY_MUTE;
+    } else if (_display_mod.net_mode == LED_NET_MODE_CONNECTED && _display_mod.denoise_state == SWITCH_OPEN) {
+        state = HL_DISPLAY_CONNECTED_DENOISE;
+    } else if (_display_mod.net_mode == LED_NET_MODE_RECONNECTION && _display_mod.denoise_state == SWITCH_OPEN) {
+        state = HL_DISPLAY_RECONNECT_DENOISE;
+    } else if (_display_mod.net_mode == LED_NET_MODE_CONNECTED) {
+        state = HL_DISPLAY_CONNECTED;
+    } else if (_display_mod.net_mode == LED_NET_MODE_RECONNECTION) {
+        state = HL_DISPLAY_RECONNECT;
+    } else {
+        state = HL_DISPLAY_IDLE;
+    }
+
+    if (state != _display_mod.display_state_1) {
+        _display_mod.display_state_1       = state;
+        _display_mod.display_update_flag_1 = true;
+    }
+}
+
+static void _display_update_1(void)
+{
+    if (_display_mod.display_update_flag_1 == false) {
+        return;
+    }
+
+    hl_drv_aw2016a_led_ctrl_st led;
+
+    switch (_display_mod.display_state_1) {
+        case HL_DISPLAY_FAULT: {
+            led.breath_mode = HL_DRV_AW2016A_BREATH_MODE_RB_FAST;
+            led.color       = HL_DRV_AW2016A_COLOR_WHITE;
+        } break;
+        case HL_DISPLAY_UPDATE: {
+            led.breath_mode = HL_DRV_AW2016A_BREATH_MODE_RB_SLOW;
+            led.color       = HL_DRV_AW2016A_COLOR_WHITE;
+        } break;
+        case HL_DISPLAY_PAIR: {
+            led.breath_mode = HL_DRV_AW2016A_BREATH_MODE_FAST;
+            led.color       = HL_DRV_AW2016A_COLOR_BLUE;
+        } break;
+        case HL_DISPLAY_LOWPOWER: {
+            led.breath_mode = HL_DRV_AW2016A_BREATH_MODE_SLOW;
+            led.color       = HL_DRV_AW2016A_COLOR_RED;
+        } break;
+        case HL_DISPLAY_MUTE: {
+            led.breath_mode = HL_DRV_AW2016A_BREATH_MODE_KEEP;
+            led.color       = HL_DRV_AW2016A_COLOR_RED;
+        } break;
+        case HL_DISPLAY_CONNECTED_DENOISE: {
+            led.breath_mode = HL_DRV_AW2016A_BREATH_MODE_KEEP;
+            led.color       = HL_DRV_AW2016A_COLOR_GREEN;
+        } break;
+        case HL_DISPLAY_RECONNECT_DENOISE: {
+            led.breath_mode = HL_DRV_AW2016A_BREATH_MODE_SLOW;
+            led.color       = HL_DRV_AW2016A_COLOR_GREEN;
+        } break;
+        case HL_DISPLAY_CONNECTED: {
+            led.breath_mode = HL_DRV_AW2016A_BREATH_MODE_KEEP;
+            led.color       = HL_DRV_AW2016A_COLOR_BLUE;
+        } break;
+        case HL_DISPLAY_RECONNECT: {
+            led.breath_mode = HL_DRV_AW2016A_BREATH_MODE_SLOW;
+            led.color       = HL_DRV_AW2016A_COLOR_BLUE;
+        } break;
+        case HL_DISPLAY_CHARGING: {
+            led.breath_mode = HL_DRV_AW2016A_BREATH_MODE_SLOW;
+            led.color       = HL_DRV_AW2016A_COLOR_ORANGE;
+        } break;
+        case HL_DISPLAY_CHARGE_FULL: {
+            led.breath_mode = HL_DRV_AW2016A_BREATH_MODE_KEEP;
+            led.color       = HL_DRV_AW2016A_COLOR_ORANGE;
+        } break;
+        default: {
+            led.breath_mode = HL_DRV_AW2016A_BREATH_MODE_SKIP;
+            led.color       = HL_DRV_AW2016A_COLOR_SKIP;
+        } break;
+    }
+
+    hl_drv_aw2016a_ctrl(HL_DRV_AW2016A_LED0, HL_DRV_AW2016A_LED_CTRL, &led, sizeof(led));
+
+    _display_mod.display_update_flag_1 = false;
+}
+
+static void _display_state_check_2(void)
+{
+    hl_mod_display_state_e state;
+
+    if (_display_mod.record_fault_state == SWITCH_OPEN) {
+        state = HL_DISPLAY_RECORD_ERR;
+    } else if (_display_mod.record_state == SWITCH_OPEN) {
+        state = HL_DISPLAY_RECORD;
+    } else if (_display_mod.record_state == SWITCH_CLOSE) {
+        state = HL_DISPLAY_RECORD_STOP;
+    } else {
+        state = HL_DISPLAY_IDLE;
+    }
+
+    if (state != _display_mod.display_state_2) {
+        _display_mod.display_state_2       = state;
+        _display_mod.display_update_flag_2 = true;
+    }
+}
+
+static void _display_update_2(void)
+{
+    if (_display_mod.display_update_flag_2 == false) {
+        return;
+    }
+
+    hl_drv_aw2016a_led_ctrl_st led;
+
+    switch (_display_mod.display_state_1) {
+        case HL_DISPLAY_RECORD_ERR: {
+            led.breath_mode = HL_DRV_AW2016A_BREATH_MODE_FAST;
+            led.color       = HL_DRV_AW2016A_COLOR_RED;
+        } break;
+        case HL_DISPLAY_RECORD: {
+            led.breath_mode = HL_DRV_AW2016A_BREATH_MODE_KEEP;
+            led.color       = HL_DRV_AW2016A_COLOR_RED;
+        } break;
+        case HL_DISPLAY_RECORD_STOP: {
+            led.breath_mode = HL_DRV_AW2016A_BREATH_MODE_KEEP;
+            led.color       = HL_DRV_AW2016A_COLOR_BLACK;
+        } break;
+        default: {
+            led.breath_mode = HL_DRV_AW2016A_BREATH_MODE_SKIP;
+            led.color       = HL_DRV_AW2016A_COLOR_SKIP;
+        } break;
+    }
+
+    hl_drv_aw2016a_ctrl(HL_DRV_AW2016A_LED1, HL_DRV_AW2016A_LED_CTRL, &led, sizeof(led));
+
+    _display_mod.display_update_flag_2 = false;
+}
+
+static void _display_thread_entry(void* arg)
+{
+    while (_display_mod.thread_exit_flag == 0) {
+        _display_state_check_1();
+        _display_update_1();  //状态灯
+
+        _display_state_check_2();
+        _display_update_2();  //录制灯
+
+        rt_thread_mdelay(100);
+    }
+
+    _display_mod.thread_exit_flag = -1;
+}
+
+/* Exported functions --------------------------------------------------------*/
+
+uint8_t hl_mod_display_init(void* display_msq)
+{
+    if (_display_mod.init_flag == true) {
+        return HL_DISPLAY_SUCCESS;
+    }
+
+    int ret;
+
+    ret = hl_drv_aw2016a_init();
+    if (ret == AW2016A_FUNC_RET_ERR) {
+        _display_mod.aw2016a_init_flag = false;
+    } else {
+        _display_mod.aw2016a_init_flag = true;
+    }
+
+    _display_mod.display_update_flag_1 = false;
+    _display_mod.display_update_flag_2 = false;
+    _display_mod.net_mode              = LED_NET_MODE_RECONNECTION;
+    _display_mod.record_state          = SWITCH_CLOSE;
+    _display_mod.record_fault_state    = SWITCH_CLOSE;
+    _display_mod.denoise_state         = SWITCH_CLOSE;
+    _display_mod.charge_state          = NOT_CHARGE;
+    _display_mod.fault_state           = SWITCH_CLOSE;
+    _display_mod.mute_state            = SWITCH_CLOSE;
+    _display_mod.update_state          = SWITCH_CLOSE;
+    _display_mod.bat_soc               = 50;
+    _display_mod.display_state_1       = HL_DISPLAY_IDLE;
+    _display_mod.display_state_2       = HL_DISPLAY_IDLE;
+    _display_mod.charging_led_count    = 0;
+    _display_mod.msg_hd                = display_msq;
+
+    _display_mod.thread_exit_flag = 0;
+
+    _display_mod.display_thread =
+        rt_thread_create("hl_mod_display_thread", _display_thread_entry, RT_NULL, 1024, 25, 10);
+    if (_display_mod.display_thread == RT_NULL) {
+        LOG_E("display thread create failed");
+        return HL_DISPLAY_FAILED;
+    }
+
+    rt_thread_startup(_display_mod.display_thread);
+
+    LOG_I("display mod init success!");
+
+    _display_mod.init_flag = true;
+
+    return HL_DISPLAY_SUCCESS;
+}
+
+uint8_t hl_mod_display_deinit(void)
+{
+    if (_display_mod.init_flag == false) {
+        return HL_DISPLAY_SUCCESS;
+    }
+
+    int ret;
+
+    _display_mod.thread_exit_flag = 1;
+
+    LOG_I("wait display thread exit");
+
+    while (_display_mod.thread_exit_flag != -1) {
+        rt_thread_mdelay(10);
+    }
+
+    LOG_I("display display deinit success!");
+
+    hl_drv_aw2016a_deinit();
+
+    LOG_I("display mod deinit success!");
+
+    _display_mod.init_flag = false;
+
+    return HL_DISPLAY_SUCCESS;
+}
+
+uint8_t hl_mod_display_io_ctrl(uint8_t cmd, void* ptr, uint16_t len)
+{
+    if (_display_mod.init_flag == false) {
+        LOG_W("display mod not init!");
+        return HL_DISPLAY_FAILED;
+    }
+
+    switch (cmd) {
+        case LED_FAULT_STATUS_READ_CMD: {
+            if (len != sizeof(uint8_t)) {
+                LOG_E("len err, ctrl arg need <uint8_t> type pointer!");
+                return HL_DISPLAY_FAILED;
+            }
+
+            if (_display_mod.aw2016a_init_flag == true) {
+                *(uint8_t*)ptr = 0;
+            } else {
+                *(uint8_t*)ptr = 1;
+            }
+        } break;
+        case LED_NET_MODLE_CMD: {
+            if (len != sizeof(hl_led_net_mode)) {
+                LOG_E("len err, ctrl arg need <hl_led_net_mode> type pointer!");
+                return HL_DISPLAY_FAILED;
+            }
+
+            _display_mod.net_mode = *(hl_led_net_mode*)ptr;
+        } break;
+        case LED_RECORD_STATE_CMD: {
+            if (len != sizeof(hl_led_switch)) {
+                LOG_E("len err, ctrl arg need <hl_led_switch> type pointer!");
+                return HL_DISPLAY_FAILED;
+            }
+
+            _display_mod.record_state = *(hl_led_switch*)ptr;
+        } break;
+        case LED_SWITCH_NOISE_REDUCTION_CMD: {
+            if (len != sizeof(hl_led_switch)) {
+                LOG_E("len err, ctrl arg need <hl_led_switch> type pointer!");
+                return HL_DISPLAY_FAILED;
+            }
+
+            _display_mod.denoise_state = *(hl_led_switch*)ptr;
+        } break;
+        case LED_CHARGE_STATUS_CMD: {
+            if (len != sizeof(hl_charge_mode)) {
+                LOG_E("len err, ctrl arg need <hl_charge_mode> type pointer!");
+                return HL_DISPLAY_FAILED;
+            }
+
+            if (_display_mod.charge_state != *(hl_charge_mode*)ptr) {
+                _display_mod.charge_state = *(hl_charge_mode*)ptr;
+                if (_display_mod.charge_state == CHARGING) {
+                    _display_mod.charging_led_count = 50;
+                }
+            }
+        } break;
+        case LED_SWITCH_FAULT_CMD: {
+            if (len != sizeof(hl_led_switch)) {
+                LOG_E("len err, ctrl arg need <hl_led_switch> type pointer!");
+                return HL_DISPLAY_FAILED;
+            }
+
+            _display_mod.fault_state = *(hl_led_switch*)ptr;
+        } break;
+        case LED_SWITCH_MUTE_CMD: {
+            if (len != sizeof(hl_led_switch)) {
+                LOG_E("len err, ctrl arg need <hl_led_switch> type pointer!");
+                return HL_DISPLAY_FAILED;
+            }
+
+            _display_mod.mute_state = *(hl_led_switch*)ptr;
+        } break;
+        case LED_SWITCH_UPDATE_CMD: {
+            if (len != sizeof(hl_led_switch)) {
+                LOG_E("len err, ctrl arg need <hl_led_switch> type pointer!");
+                return HL_DISPLAY_FAILED;
+            }
+
+            _display_mod.update_state = *(hl_led_switch*)ptr;
+        } break;
+        case LED_BATTERY_VAL_CMD: {
+            if (len != sizeof(uint8_t)) {
+                LOG_E("len err, ctrl arg need <uint8_t> type pointer!");
+                return HL_DISPLAY_FAILED;
+            }
+
+            _display_mod.bat_soc = *(uint8_t*)ptr;
+        } break;
+        case LED_BRIGHT_SET_CMD: {
+            if (len != sizeof(uint8_t)) {
+                LOG_E("len err, ctrl arg need <uint8_t> type pointer!");
+                return HL_DISPLAY_FAILED;
+            }
+
+            _display_mod.led_brightness = *(uint8_t*)ptr;
+        } break;
+        case LED_RECORD_FAULT_CMD: {
+            if (len != sizeof(hl_led_switch)) {
+                LOG_E("len err, ctrl arg need <hl_led_switch> type pointer!");
+                return HL_DISPLAY_FAILED;
+            }
+
+            _display_mod.record_fault_state = *(hl_led_switch*)ptr;
+        } break;
+        default:
+            break;
+    }
+
+    return HL_DISPLAY_SUCCESS;
+}
+
+#endif
+/*
+ * EOF
+ */
