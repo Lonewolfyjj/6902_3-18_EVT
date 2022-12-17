5740e387976e136963a100e051053aaf820e4f04
yangxianyun <rd52@hollyland-tech.com>
2022-12-17 09:25:36 +0000

    🔧 修改（音量）：开机音量设置  1. 修改只有RX才有开机的音量设置 结束

--- applications/HL_A6902/app/src/hl_app_mng.c
+++ applications/HL_A6902/app/src/hl_app_mng.c
@@ -166,7 +166,10 @@ void hl_app_mng_powerOn(void)
     hl_mod_euc_init(&hl_app_mq);
     hl_mod_euc_start();
 
+#if HL_IS_TX_DEVICE()
+#else
     hl_mod_audio_io_ctrl(HL_AUDIO_SET_GAIN_CMD, &rx_info.cur_volume_r, 2);
+#endif
 }
 
 // 关机，去初始化模块
