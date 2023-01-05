21dc9f221f9dca54b006b673bd42bcdd8389e4c4
lilin <lin.li@hollyland-tech.com>
2023-01-05 11:22:30 +0000

    ✨ 增加功能（app）：Rx 收纳盒交互显示   1、增加Rx 与收纳盒交互的显示 结束

--- applications/HL_A6902/app/src/hl_app_com_msg_pro.c
+++ applications/HL_A6902/app/src/hl_app_com_msg_pro.c
@@ -28,6 +28,7 @@
 #include "hl_app_mng.h"
 
 #include "hl_mod_euc.h"
+#include "hl_mod_display.h"
 
 #define DBG_SECTION_NAME "app_com"
 #define DBG_LEVEL DBG_LOG
@@ -36,7 +37,32 @@
 /* typedef -------------------------------------------------------------------*/
 /* define --------------------------------------------------------------------*/
 /* variables -----------------------------------------------------------------*/
+
+static bool _rx_in_box_flag = false;
+static bool _tx1_in_box_flag = false;
+static bool _tx2_in_box_flag = false;
+
 /* Private function(only *.c)  -----------------------------------------------*/
+
+static void _display_in_box_state_set(void)
+{
+    hl_display_box_charge_state display_box_charge_state;
+
+    if (_rx_in_box_flag == false) {
+        display_box_charge_state = BOX_CHARGE_RX_NOT;
+    } else if (_tx1_in_box_flag == false && _tx2_in_box_flag == false) {
+        display_box_charge_state = BOX_CHARGE_RX;
+    } else if (_tx1_in_box_flag == true && _tx2_in_box_flag == true) {
+        display_box_charge_state = BOX_CHARGE_RX_TX12;
+    } else if (_tx1_in_box_flag == true) {
+        display_box_charge_state = BOX_CHARGE_RX_TX1;
+    } else {
+        display_box_charge_state = BOX_CHARGE_RX_TX2;
+    }
+
+    hl_mod_display_io_ctrl(IN_BOX_STATE_VAL_CMD, &display_box_charge_state, sizeof(display_box_charge_state));
+}
+
 /* Exported functions --------------------------------------------------------*/
 #if HL_IS_TX_DEVICE()
 void hl_app_com_msg_pro(mode_to_app_msg_t* p_msg)
@@ -99,12 +125,18 @@ void hl_app_com_msg_pro(mode_to_app_msg_t* p_msg)
     hl_mod_euc_rtc_st          rtc_time_temp = { 0 };
     hl_mod_euc_box_lid_state_e box_lid_state_temp;
 
+    uint8_t temp;
+
     switch (p_msg->cmd) {
         case HL_IN_BOX_IND: {
             LOG_I("in box!");
+            _rx_in_box_flag = true;
+            _display_in_box_state_set();
         } break;
         case HL_OUT_BOX_IND: {
             LOG_I("out box!");
+            _rx_in_box_flag = false;
+            _display_in_box_state_set();
         } break;
         case HL_GET_SOC_REQ_IND: {  // 请求获取电池电量
             bat_soc_temp = rx_info.soc;
@@ -113,21 +145,31 @@ void hl_app_com_msg_pro(mode_to_app_msg_t* p_msg)
         case HL_TX1_BAT_INFO_UPDATE_IND: {  //更新tx1电池电量
             tx1_bat_info_temp = *(uint8_t*)p_msg->param.ptr;
             LOG_I("Tx1 bat soc update: %d", tx1_bat_info_temp);
+
+            hl_mod_display_io_ctrl(TX1_BAT_VAL_VAL_CMD, &tx1_bat_info_temp, sizeof(tx1_bat_info_temp));
         } break;
         case HL_TX2_BAT_INFO_UPDATE_IND: {  //更新tx2电池电量
             tx2_bat_info_temp = *(uint8_t*)p_msg->param.ptr;
             LOG_I("Tx2 bat soc update: %d", tx2_bat_info_temp);
+
+            hl_mod_display_io_ctrl(TX2_BAT_VAL_VAL_CMD, &tx2_bat_info_temp, sizeof(tx2_bat_info_temp));
         } break;
         case HL_BOX_BAT_INFO_UPDATE_IND: {  //更新box电池电量
             box_bat_info_temp = *(uint8_t*)p_msg->param.ptr;
             LOG_I("Box bat soc update: %d", box_bat_info_temp);
+
+            hl_mod_display_io_ctrl(CASE_BAT_VAL_VAL_CMD, &box_bat_info_temp, sizeof(box_bat_info_temp));
         } break;
         case HL_TX1_IN_BOX_STATE_IND: {  //更新Tx1在盒状态
             tx1_in_box_flag_temp = *(bool*)p_msg->param.ptr;
+            _tx1_in_box_flag = tx1_in_box_flag_temp;
+            _display_in_box_state_set();
             LOG_I("Tx1 in box state:%d", tx1_in_box_flag_temp);
         } break;
         case HL_TX2_IN_BOX_STATE_IND: {  //更新Tx2在盒状态
             tx2_in_box_flag_temp = *(bool*)p_msg->param.ptr;
+            _tx2_in_box_flag = tx2_in_box_flag_temp;
+            _display_in_box_state_set();
             LOG_I("Tx2 in box state:%d", tx2_in_box_flag_temp);
         } break;
         case HL_SET_PAIR_MAC_TX1_REQ_IND: {  //请求设置tx1的mac地址来配对
@@ -166,14 +208,35 @@ void hl_app_com_msg_pro(mode_to_app_msg_t* p_msg)
         case HL_TX1_CHARGE_STATE_IND: {  //更新Tx1的充电状态
             tx1_charge_state_temp = *(hl_mod_euc_charge_state_e*)p_msg->param.ptr;
             LOG_I("Tx1 charge state:%d", tx1_charge_state_temp);
+
+            if (tx1_charge_state_temp == HL_MOD_EUC_CHARGE_STATE_CHARGING) {
+                temp = 1;
+            } else {
+                temp = 0;
+            }
+            hl_mod_display_io_ctrl(TX1_CHARGE_STATUS_SWITCH_CMD, &temp, sizeof(temp));
         } break;
         case HL_TX2_CHARGE_STATE_IND: {  //更新Tx2的充电状态
             tx2_charge_state_temp = *(hl_mod_euc_charge_state_e*)p_msg->param.ptr;
             LOG_I("Tx2 charge state:%d", tx2_charge_state_temp);
+
+            if (tx2_charge_state_temp == HL_MOD_EUC_CHARGE_STATE_CHARGING) {
+                temp = 1;
+            } else {
+                temp = 0;
+            }
+            hl_mod_display_io_ctrl(TX2_CHARGE_STATUS_SWITCH_CMD, &temp, sizeof(temp));
         } break;
         case HL_BOX_CHARGE_STATE_IND: {  //更新Box的充电状态
             box_charge_state_temp = *(hl_mod_euc_charge_state_e*)p_msg->param.ptr;
             LOG_I("Box charge state:%d", box_charge_state_temp);
+
+            if (box_charge_state_temp == HL_MOD_EUC_CHARGE_STATE_CHARGING) {
+                temp = 1;
+            } else {
+                temp = 0;
+            }
+            hl_mod_display_io_ctrl(BOX_CHARGE_STATUS_SWITCH_CMD, &temp, sizeof(temp));
         } break;
         case HL_GET_RTC_TIME_REQ_IND: {  //请求设置RTC时间
             hl_mod_euc_ctrl(HL_SET_RTC_TIME_CMD, &rtc_time_temp, sizeof(rtc_time_temp));
