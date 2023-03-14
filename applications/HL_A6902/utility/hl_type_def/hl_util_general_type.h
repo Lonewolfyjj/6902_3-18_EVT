5436eff8949b5c135088f0fdead1ddea55008643
lisonglin <songlin.li@hollyland-tech.com>
2023-03-14 11:51:41 +0000

    🎨 改进（cmd）：APP命令  1、修改LarkSoundAPP控制命令 结束

--- applications/HL_A6902/utility/hl_type_def/hl_util_general_type.h
+++ applications/HL_A6902/utility/hl_type_def/hl_util_general_type.h
@@ -102,6 +102,70 @@ typedef struct _hl_audio_time_t_
     uint8_t second;
 } __attribute__((packed, aligned(1))) hl_audio_time_t;
 
+typedef enum _larksound_ctrl_cmd_enum_
+{
+    /// 获取软件版本号
+    LARKSOUND_GET_SOFT_VERSION_CMD  = 0x01,
+    /// 获取硬件版本号
+    LARKSOUND_GET_HARD_VERSION_CMD  = 0x02,
+    /// 获取设备连接状态
+    LARKSOUND_GET_CONTECT_STATE_CMD = 0x03,
+    /// 获取设备MAC地址
+    LARKSOUND_GET_MAC_CMD           = 0x04,
+    /// 获取设备电量
+    LARKSOUND_GET_SOC_CMD           = 0x05,
+    /// 获取设备UV值
+    LARKSOUND_GET_UV_CMD            = 0x06,
+    /// 获取设备ID
+    LARKSOUND_GET_DEV_NUB_CMD       = 0x07,
+    /// 获取TX增益
+    LARKSOUND_GET_TX_GAIN_CMD       = 0x21,
+    /// 设置TX增益
+    LARKSOUND_SET_TX_GAIN_CMD       = 0x22,
+    /// 获取降噪等级
+    LARKSOUND_GET_DENOISE_CMD       = 0x23,
+    /// 设置降噪等级
+    LARKSOUND_SET_DENOISE_CMD       = 0x24,
+    /// 获取低切状态
+    LARKSOUND_GET_LOWCUT_CMD        = 0x25,
+    /// 设置低切状态
+    LARKSOUND_SET_LOWCUT_CMD        = 0x26,
+    /// 获取MUTE状态
+    LARKSOUND_GET_MUTE_CMD          = 0x27,
+    /// 设置MUTE状态
+    LARKSOUND_SET_MUTE_CMD          = 0x28,
+    /// 获取音效模式
+    LARKSOUND_GET_SOUND_EFFECT_CMD  = 0x29,
+    /// 设置音效模式
+    LARKSOUND_SET_SOUND_EFFECT_CMD  = 0x2A,
+    /// 获取手机外放状态
+    LARKSOUND_GET_PHONE_OUT_CMD     = 0x2B,
+    /// 设置手机外放状态
+    LARKSOUND_SET_PHONE_OUT_CMD     = 0x2C,
+    /// 获取UAC增益
+    LARKSOUND_GET_UAC_GAIN_CMD      = 0x2D,
+    /// 设置UAC增益
+    LARKSOUND_SET_UAC_GAIN_CMD      = 0x2E,
+    /// 获取RX监听口增益
+    LARKSOUND_GET_RX_GAIN_CMD       = 0x2F,
+    /// 设置RX监听口增益
+    LARKSOUND_SET_RX_GAIN_CMD       = 0x30,
+    /// 获取相机口增益
+    LARKSOUND_GET_CAM_GAIN_CMD      = 0x31,
+    /// 设置相机口增益
+    LARKSOUND_SET_CAM_GAIN_CMD      = 0x32,
+    /// 开始升级
+    LARKSOUND_UPDATE_START_CMD      = 0x41,
+    /// 传输升级包
+    LARKSOUND_UPDATE_PACKET_CMD     = 0x42,
+    /// 结束升级
+    LARKSOUND_UPDATE_STOP_CMD       = 0x43,
+    /// 维持心跳包
+    LARKSOUND_KEEP_ALIVE_CMD        = 0x61,
+    /// 设备重启
+    LARKSOUND_REBOOT_CMD            = 0x62,
+} HL_ENUM8(hl_larksound_ctrl_cmd_e);
+
 /* define --------------------------------------------------------------------*/
 /* variables -----------------------------------------------------------------*/
 /* Private function(only *.c)  -----------------------------------------------*/
