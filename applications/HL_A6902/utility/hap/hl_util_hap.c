4fc565eef2fee61b355cc24b4b91df338e11ee30
lisonglin <songlin.li@hollyland-tech.com>
2023-03-14 07:43:52 +0000

    📝 添加（util）：工具  1、添加HAP应用程序通信协议  2、创建HAP文件夹等文件 结束

---
+++ applications/HL_A6902/utility/hap/hl_util_hap.c
@@ -0,0 +1,175 @@
+#include "hl_util_hap.h"
+#include "hl_util_xor_calc.h"
+#include <stdlib.h>
+#include <string.h>
+
+int hl_util_hap_init(hl_util_hap_t* hap_ptr, uint8_t* frame_data_addr, uint32_t (*get_time_stamp)(void),
+                     void (*hap_success_handle_cb)(hap_protocol_type_t))
+{
+    if (NULL == hap_success_handle_cb) {
+        HL_UTIL_HAP_DBG("[ERROR] hap init hap_success_handle_cb arg is NULL\n");
+        return -1;
+    }
+
+    // 检查hap_handle结构体是否赋予初始值
+    if ((hap_ptr->hap_handle.role != EM_HAP_ROLE_MASTER) && (hap_ptr->hap_handle.role != EM_HAP_ROLE_SLAVE)) {
+        HL_UTIL_HAP_DBG("[ERROR] hap init handle role arg no init value\n");
+        return -1;
+    }
+    if ((hap_ptr->hap_handle.timer_state != EM_HAP_TIMER_ENABLE)
+        && (hap_ptr->hap_handle.timer_state != EM_HAP_TIMER_DISABLE)) {
+        HL_UTIL_HAP_DBG("[ERROR] hap init handle timer_state arg no init value\n");
+        return -1;
+    }
+    if (hap_ptr->hap_handle.frame_data_len == 0) {
+        HL_UTIL_HAP_DBG("[ERROR] hap init handle frame_data_len arg no init value\n");
+        return -1;
+    }
+
+    // 初始化hap结构体变量值
+    hap_ptr->state                 = EM_HAP_STATE_HDR_H;
+    hap_ptr->max_timeout           = HAP_MAX_TIMEOUT;
+    hap_ptr->get_time_stamp        = get_time_stamp;
+    hap_ptr->hap_success_handle_cb = hap_success_handle_cb;
+    hap_ptr->hap_frame.data_addr   = frame_data_addr;
+
+    return 0;
+}
+
+int hl_util_hap_deinit(hl_util_hap_t* hap_ptr)
+{
+    if (NULL == hap_ptr) {
+        return -1;
+    }
+
+    hap_ptr->get_time_stamp        = NULL;
+    hap_ptr->hap_success_handle_cb = NULL;
+
+    return 0;
+}
+
+int hl_util_hap_encode(hap_role_em role, uint8_t cmd, uint8_t* frame_buf, uint16_t buf_len, uint8_t* data_addr,
+                       uint16_t data_len)
+{
+    if ((NULL == frame_buf) | (NULL == data_addr)) {
+        HL_UTIL_HAP_DBG("[ERROR] hap protocol encode arg\n");
+        return -1;
+    } else if (buf_len < (data_len + 6)) {
+        HL_UTIL_HAP_DBG("[ERROR] hap protocol encode arg\n");
+        return -1;
+    }
+
+    if (role == EM_HAP_ROLE_MASTER) {
+        frame_buf[0] = HAP_FRAME_HOST_HDR_H;
+        frame_buf[1] = HAP_FRAME_HOST_HDR_L;
+    } else if (role == EM_HAP_ROLE_SLAVE) {
+        frame_buf[0] = HAP_FRAME_CLIENT_HDR_H;
+        frame_buf[1] = HAP_FRAME_CLIENT_HDR_L;
+    }
+    frame_buf[2] = cmd;
+    frame_buf[3] = (uint8_t)(0xFF & (data_len >> 8));
+    frame_buf[4] = (uint8_t)(0xFF & data_len);
+    memcpy(&frame_buf[5], data_addr, data_len);
+
+    frame_buf[5 + data_len] = hl_util_xor_calc_xor8(frame_buf, data_len + 5);
+
+    return data_len + 6;
+}
+
+int hl_util_hap_decode(hl_util_hap_t* hap_ptr, uint8_t data_byte)
+{
+    if (NULL == hap_ptr) {
+        HL_UTIL_HAP_DBG("[ERROR] hap protocol decode arg\n");
+        return -1;
+    }
+
+    if (hap_ptr->hap_handle.timer_state == EM_HAP_TIMER_ENABLE) {
+        if (hap_ptr->state == EM_HAP_STATE_HDR_H) {
+            hap_ptr->cur_timeout_val  = 0;
+            hap_ptr->last_timeout_val = 0;
+        } else {
+            hap_ptr->cur_timeout_val = hap_ptr->get_time_stamp();
+        }
+
+        if ((hap_ptr->cur_timeout_val - hap_ptr->last_timeout_val) > hap_ptr->max_timeout) {
+            hap_ptr->state = EM_HAP_STATE_HDR_H;
+            HL_UTIL_HAP_DBG("[INFO] hap protocol decode reset\n");
+        }
+    }
+
+    switch (hap_ptr->state) {
+        case EM_HAP_STATE_HDR_H:
+            if (((hap_ptr->hap_handle.role == EM_HAP_ROLE_MASTER) && (data_byte == HAP_FRAME_CLIENT_HDR_H))
+                || ((hap_ptr->hap_handle.role == EM_HAP_ROLE_SLAVE) && (data_byte == HAP_FRAME_HOST_HDR_H))) {
+                hap_ptr->hap_frame.hdr_h = data_byte;
+                hap_ptr->state           = EM_HAP_STATE_HDR_L;
+                hap_ptr->xor8            = data_byte;
+                hap_ptr->read_data_len   = 0;
+                if (hap_ptr->hap_handle.timer_state == EM_HAP_TIMER_ENABLE) {
+                    hap_ptr->last_timeout_val = hap_ptr->get_time_stamp();
+                }
+            }
+            break;
+
+        case EM_HAP_STATE_HDR_L:
+            if (((hap_ptr->hap_handle.role == EM_HAP_ROLE_MASTER) && (data_byte == HAP_FRAME_CLIENT_HDR_L))
+                || ((hap_ptr->hap_handle.role == EM_HAP_ROLE_SLAVE) && (data_byte == HAP_FRAME_HOST_HDR_L))) {
+                hap_ptr->hap_frame.hdr_l = data_byte;
+                hap_ptr->state           = EM_HAP_STATE_CMD;
+                hap_ptr->xor8            = hap_ptr->xor8 ^ data_byte;
+            } else {
+                hap_ptr->state = EM_HAP_STATE_HDR_H;
+            }
+            break;
+
+        case EM_HAP_STATE_CMD:
+            hap_ptr->hap_frame.cmd = data_byte;
+            hap_ptr->state         = EM_HAP_STATE_CTRL;
+            hap_ptr->xor8          = hap_ptr->xor8 ^ data_byte;
+            break;
+
+        case EM_HAP_STATE_CTRL:
+            hap_ptr->hap_frame.ctrl = data_byte;
+            hap_ptr->state          = EM_HAP_STATE_DATA_LEN_H;
+            hap_ptr->xor8           = hap_ptr->xor8 ^ data_byte;
+            break;
+
+        case EM_HAP_STATE_DATA_LEN_H:
+            hap_ptr->hap_frame.data_len_h = data_byte;
+            hap_ptr->frame_data_len       = (data_byte << 8);
+            hap_ptr->state                = EM_HAP_STATE_DATA_LEN_L;
+            hap_ptr->xor8                 = hap_ptr->xor8 ^ data_byte;
+            break;
+
+        case EM_HAP_STATE_DATA_LEN_L:
+            hap_ptr->hap_frame.data_len_l = data_byte;
+            hap_ptr->frame_data_len       = hap_ptr->frame_data_len | data_byte;
+            hap_ptr->xor8                 = hap_ptr->xor8 ^ data_byte;
+            if (0 == hap_ptr->frame_data_len) {
+                hap_ptr->state = EM_HAP_STATE_CRC;
+            } else {
+                hap_ptr->state = EM_HAP_STATE_DATA;
+            }
+            break;
+
+        case EM_HAP_STATE_DATA:
+            hap_ptr->hap_frame.data_addr[hap_ptr->read_data_len++] = data_byte;
+            hap_ptr->xor8                                          = hap_ptr->xor8 ^ data_byte;
+            if (hap_ptr->read_data_len == hap_ptr->frame_data_len) {
+                hap_ptr->state = EM_HAP_STATE_CRC;
+            }
+            break;
+
+        case EM_HAP_STATE_CRC:
+            hap_ptr->hap_frame.xor8 = data_byte;
+            hap_ptr->state          = EM_HAP_STATE_HDR_H;
+            if (hap_ptr->hap_frame.xor8 == hap_ptr->xor8) {
+                hap_ptr->hap_success_handle_cb(hap_ptr->hap_frame);
+            }
+            break;
+
+        default:
+            break;
+    }
+    return 0;
+}
---
+++ applications/HL_A6902/utility/hap/hl_util_hap.h
@@ -0,0 +1,238 @@
+/**
+ * @file hl_util_hap.h
+ * @author lisonglin (songlin.li@hollyland-tech.com)
+ * @brief APP协议解析处理文件
+ * @version 1.0
+ * @date 2023-03-13
+ * 
+ * ██╗  ██╗ ██████╗ ██╗     ██╗  ██╗   ██╗██╗      █████╗ ███╗   ██╗██████╗ 
+ * ██║  ██║██╔═══██╗██║     ██║  ╚██╗ ██╔╝██║     ██╔══██╗████╗  ██║██╔══██╗
+ * ███████║██║   ██║██║     ██║   ╚████╔╝ ██║     ███████║██╔██╗ ██║██║  ██║
+ * ██╔══██║██║   ██║██║     ██║    ╚██╔╝  ██║     ██╔══██║██║╚██╗██║██║  ██║
+ * ██║  ██║╚██████╔╝███████╗███████╗██║   ███████╗██║  ██║██║ ╚████║██████╔╝
+ * ╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚══════╝╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝
+ * @copyright Copyright (c) 2023 hollyland
+ * 
+ * @par 修改日志:
+ * <table>
+ * <tr><th>Date           <th>Version  <th>Author         <th>Description
+ * <tr><td>2023-03-13     <td>v1.0     <td>lisonglin     <td>内容
+ * </table>
+ * 
+ */
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __HL_UTIL_HAP_H__
+#define __HL_UTIL_HAP_H__
+
+/* Includes ------------------------------------------------------------------*/
+#include <stdint.h>
+#include <stdio.h>
+#include <rtthread.h>
+
+/* typedef -------------------------------------------------------------------*/
+typedef enum
+{
+    EM_HAP_STATE_HDR_H,
+    EM_HAP_STATE_HDR_L,
+    EM_HAP_STATE_CMD,
+    EM_HAP_STATE_CTRL,
+    EM_HAP_STATE_DATA_LEN_H,
+    EM_HAP_STATE_DATA_LEN_L,
+    EM_HAP_STATE_DATA,
+    EM_HAP_STATE_CRC,
+} hap_protocol_state_em;
+
+typedef enum
+{
+    EM_HAP_ROLE_MASTER = 0x11,
+    EM_HAP_ROLE_SLAVE,
+} hap_role_em;
+
+typedef enum
+{
+    EM_HAP_TIMER_ENABLE = 0x21,
+    EM_HAP_TIMER_DISABLE,
+} hap_timer_state_em;
+
+typedef struct
+{
+    /// 协议头部高字节
+    uint8_t hdr_h;
+    /// 协议头部低字节
+    uint8_t hdr_l;
+    /// 命令位
+    uint8_t cmd;
+    /// 控制位
+    uint8_t ctrl;
+    /// 数据内容长度高字节
+    uint8_t data_len_h;
+    /// 数据内容长度低字节
+    uint8_t data_len_l;
+    /// 数据内容地址
+    uint8_t* data_addr;
+    /// 异或校验位
+    uint8_t xor8;
+} hap_protocol_type_t;
+
+typedef struct
+{
+    /// 协议角色
+    hap_role_em role;
+    /// 定时器使能状态
+    hap_timer_state_em timer_state;
+    /// 已申请存放数据缓冲区长度
+    uint16_t frame_data_len;
+} hap_handle_type_t;
+
+typedef struct
+{
+    /// 异或校验值
+    uint8_t xor8;
+    /// 状态机当前状态
+    uint8_t state;
+    /// 记录当前帧的数据长度
+    uint16_t frame_data_len;
+    /// 记录当前已解析的数据长度
+    uint16_t read_data_len;
+    /// 上次时间戳值
+    uint32_t last_timeout_val;
+    /// 当前时间戳值
+    uint32_t cur_timeout_val;
+    /// 最大超时时间
+    uint32_t max_timeout;
+    /// hap句柄结构体
+    hap_handle_type_t hap_handle;
+    /// hap协议帧结构体
+    hap_protocol_type_t hap_frame;
+    /**
+     * get_time_stamp
+     * @brief 获取当前时间戳
+     * @date 2023-03-13
+     * @author lisonglin (songlin.li@hollyland-tech.com)
+     * 
+     * @details 
+     * @note 
+     * @par 修改日志:
+     * <table>
+     * <tr><th>Date             <th>Author         <th>Description
+     * <tr><td>2023-03-13      <td>lisonglin     <td>新建
+     * </table>
+     */
+    uint32_t (*get_time_stamp)(void);
+    /**
+     * hap_success_handle_cb
+     * @brief hap解析成功后的处理函数
+     * @date 2023-03-13
+     * @author lisonglin (songlin.li@hollyland-tech.com)
+     * 
+     * @details 
+     * @note 
+     * @par 修改日志:
+     * <table>
+     * <tr><th>Date             <th>Author         <th>Description
+     * <tr><td>2023-03-13      <td>lisonglin     <td>新建
+     * </table>
+     */
+    void (*hap_success_handle_cb)(hap_protocol_type_t);
+} hl_util_hap_t;
+
+/* define --------------------------------------------------------------------*/
+#define HL_UTIL_HAP_DBG rt_kprintf
+#define HAP_MAX_TIMEOUT 500
+
+#define HAP_FRAME_HOST_HDR_H 0xAA
+#define HAP_FRAME_HOST_HDR_L 0xDD
+#define HAP_FRAME_CLIENT_HDR_H 0xBB
+#define HAP_FRAME_CLIENT_HDR_L 0xDD
+
+/* variables -----------------------------------------------------------------*/
+/* Private function(only *.c)  -----------------------------------------------*/
+/**
+ * hl_util_hap_init
+ * @brief 初始化hap协议帧结构体，申请内存空间，注册回调函数
+ * @param [in] hap_ptr hap结构体指针(传入该参数前，需对成员变量hap_handle结构体赋初值)
+ * @param [in] frame_data_addr 协议帧数据存放缓冲区
+ * @param [in] get_time_stamp 获取当前时间戳（函数指针）
+ * @param [in] hap_success_handle_cb 解析成功后的处理（函数指针）
+ * @return int 成功 0 | 失败 -1
+ * @date 2023-03-13
+ * @author lisonglin (songlin.li@hollyland-tech.com)
+ * 
+ * @details 
+ * @note 
+ * @par 修改日志:
+ * <table>
+ * <tr><th>Date             <th>Author         <th>Description
+ * <tr><td>2023-03-13      <td>lisonglin     <td>新建
+ * </table>
+ */
+int hl_util_hap_init(hl_util_hap_t* hap_ptr, uint8_t* frame_data_addr, uint32_t (*get_time_stamp)(void),
+                     void (*hap_success_handle_cb)(hap_protocol_type_t));
+
+/**
+ * hl_util_hap_deinit
+ * @brief hap协议帧结构体去初始化，释放内存空间
+ * @param [in] hap_ptr hap结构体指针
+ * @return int 成功 0 | 失败 -1
+ * @date 2023-03-13
+ * @author lisonglin (songlin.li@hollyland-tech.com)
+ * 
+ * @details 
+ * @note 
+ * @par 修改日志:
+ * <table>
+ * <tr><th>Date             <th>Author         <th>Description
+ * <tr><td>2023-03-13      <td>lisonglin     <td>新建
+ * </table>
+ */
+int hl_util_hap_deinit(hl_util_hap_t* hap_ptr);
+
+/**
+ * hl_util_hap_encode
+ * @brief hap协议封包函数
+ * @param [in] role hap当前角色
+ * @param [in] cmd 控制命令
+ * @param [in] frame_buf 存放hap协议包的缓冲区
+ * @param [in] buf_len 缓冲区大小
+ * @param [in] data_addr 数据内容地址
+ * @param [in] data_len 数据内容长度
+ * @return int 成功 数据包长度 | 失败 -1
+ * @date 2023-03-13
+ * @author lisonglin (songlin.li@hollyland-tech.com)
+ * 
+ * @details 
+ * @note 
+ * @par 修改日志:
+ * <table>
+ * <tr><th>Date             <th>Author         <th>Description
+ * <tr><td>2023-03-13      <td>lisonglin     <td>新建
+ * </table>
+ */
+int hl_util_hap_encode(hap_role_em role, uint8_t cmd, uint8_t* frame_buf, uint16_t buf_len, uint8_t* data_addr,
+                       uint16_t data_len);
+
+/**
+ * hl_util_hap_decode
+ * @brief hap协议解包函数
+ * @param [in] hap_ptr hap结构体指针
+ * @param [in] data_byte 解析单个字节
+ * @return int 成功 0 | 失败 -1
+ * @date 2023-03-13
+ * @author lisonglin (songlin.li@hollyland-tech.com)
+ * 
+ * @details 
+ * @note 
+ * @par 修改日志:
+ * <table>
+ * <tr><th>Date             <th>Author         <th>Description
+ * <tr><td>2023-03-13      <td>lisonglin     <td>新建
+ * </table>
+ */
+int hl_util_hap_decode(hl_util_hap_t* hap_ptr, uint8_t data_byte);
+
+#endif
+
+/* Exported functions --------------------------------------------------------*/
+/*
+ * EOF
+ */
